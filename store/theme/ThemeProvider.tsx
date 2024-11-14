16cb02adf9671d87bb167012a732c29cf9843424
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-14 11:24:49 +0100

    Add theme provider

--- store/theme/ThemeProvider.tsx
+++ store/theme/ThemeProvider.tsx
@@ -1,1 +1,48 @@
 'use client';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+
+type ThemeContext = {
+  mode: string;
+  onChange?: (mode: string) => void;
+};
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+};
+
+export const ThemeContext = createContext<ThemeContext>({ mode: 'light' });
+
+export const useTheme = () => {
+  const theme = useContext(ThemeContext);
+
+  if (!theme) {
+    throw new Error('Place is not wrapped by Theme provider');
+  }
+
+  return theme;
+};
+
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [theme, setTheme] = useState({ mode: 'light' });
+
+  useEffect(() => {
+    const themeLocal = JSON.parse(localStorage.getItem('mode')!) || 'light';
+    setTheme({ mode: themeLocal });
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      ...theme,
+      onChange: (mode: string) => {
+        setTheme({ mode });
+      },
+    }),
+    [theme]
+  );
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
