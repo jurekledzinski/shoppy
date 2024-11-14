f6e5ead241607569d12acb97516852a30a771793
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-14 20:28:51 +0100

    Add open aside on click cart menu buttons

--- components/shared/header-buttons/CartButton.tsx
+++ components/shared/header-buttons/CartButton.tsx
@@ -1,11 +1,34 @@
 'use client';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import styles from './CardButton.module.css';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useAside } from '@/store/aside';
 
 const CartButton = () => {
+  const context = useAside();
+
   return (
-    <button className={styles.cartButton}>
+    <button
+      className={styles.cartButton}
+      onClick={() => {
+        const actionElement = context.type;
+        const stateOpen = context.value;
+
+        if (actionElement === 'menu' && stateOpen) {
+          context.onChange('menu', !stateOpen);
+
+          const idTimeout = setTimeout(() => {
+            context.onChange('cart', true);
+
+            clearTimeout(idTimeout);
+          }, 1000);
+
+          return;
+        }
+
+        context.onChange('cart', !stateOpen);
+      }}
+    >
       <FontAwesomeIcon icon={faCartShopping} />
     </button>
   );
--- components/shared/header-buttons/MenuButton.tsx
+++ components/shared/header-buttons/MenuButton.tsx
@@ -1,11 +1,32 @@
 'use client';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useAside } from '@/store/aside';
 import styles from './MenuButton.module.css';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const MenuButton = () => {
+  const context = useAside();
+
   return (
-    <button className={styles.menuButton}>
+    <button
+      className={styles.menuButton}
+      onClick={() => {
+        const actionElement = context.type;
+        const stateOpen = context.value;
+        if (actionElement === 'cart' && stateOpen) {
+          context.onChange('cart', !stateOpen);
+
+          const idTimeout = setTimeout(() => {
+            context.onChange('menu', true);
+            clearTimeout(idTimeout);
+          }, 1000);
+
+          return;
+        }
+
+        context.onChange('menu', !stateOpen);
+      }}
+    >
       <FontAwesomeIcon icon={faBars} />
     </button>
   );
