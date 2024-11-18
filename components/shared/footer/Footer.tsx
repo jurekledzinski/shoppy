63f20d148ebcf7d00d93dcf1c56bd922661c6df2
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-18 13:09:24 +0100

    Add footer component

--- components/shared/footer/Footer.tsx
+++ components/shared/footer/Footer.tsx
@@ -1,11 +1,9 @@
 import styles from './Footer.module.css';
 
-const Footer = () => {
+export const Footer = () => {
   return (
     <footer className={styles.footer}>
       All rights reserved © {new Date().getFullYear()}
     </footer>
   );
 };
-
-export default Footer;
