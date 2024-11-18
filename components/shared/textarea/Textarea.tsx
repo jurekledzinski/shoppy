129b5491e23335441c3243a5173e86df0ac58fdf
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-18 13:13:52 +0100

    Create textarea component

---
+++ components/shared/textarea/Textarea.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { TextareaProps } from './types';
+import styles from './Textarea.module.css';
+
+export const Textarea = ({ className, ...props }: TextareaProps) => (
+  <textarea
+    {...props}
+    className={[className, styles.textarea].filter(Boolean).join(' ')}
+  />
+);
