f31be92338db717d2935e58bbfd282d995ca2b47
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-18 13:00:52 +0100

    Create contact action

--- actions/contact.ts
+++ actions/contact.ts
@@ -4,20 +4,13 @@
 
 export const contact = async (prevState: unknown, formData: FormData) => {
   const body = Object.fromEntries(formData);
-  console.log('action data contact', body);
 
   const res = await fetch('http://localhost:3000/api/v1/contact', {
     body: JSON.stringify(body),
     method: 'POST',
   });
 
-  const data = await res.json();
-
-  console.log('res action data contact', data);
-
   if (!res.ok) {
-    return { message: 'Please enter a valid name' };
+    return { message: 'Something went wrong, please try later' };
   }
-
-  return { ok: 'hello' };
 };
