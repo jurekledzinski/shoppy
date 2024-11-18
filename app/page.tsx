22f19d5ee264b2fd55c5fa36005eccaa664ab42a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-18 18:55:58 +0100

    Add to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import styles from '@styles/HomePage.module.css';
 
 export default function Home() {
-  return <div className={styles.home}>Home page</div>;
+  return (
+    <div className={styles.home}>
+      <div className={styles.slider}></div>
+      <div className={styles.brands}></div>
+    </div>
+  );
 }
