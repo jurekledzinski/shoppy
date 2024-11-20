e3e249108f9b42de4de9fb8b66e0d3f6e1574dc3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-20 11:52:29 +0100

    Add zod models

---
+++ models/user.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+
+export const UserSchema = z.object({
+  _id: z.string().optional(),
+  name: z
+    .string({
+      required_error: 'Name is required',
+      invalid_type_error: 'Name must be a text',
+    })
+    .min(1, { message: 'Name is required' }),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .email({ message: 'Email is invalid' })
+    .min(1, { message: 'Email is required' }),
+  password: z
+    .string({ required_error: 'Password is required' })
+    .min(8, { message: 'Password required at least 8 characters' }),
+});
+
+export const LoginUserSchema = UserSchema.pick({
+  _id: true,
+  email: true,
+  password: true,
+});
+
+export const ChangeUserPasswordSchema = UserSchema.pick({ password: true });
+export const UpdateUserProfileSchema = UserSchema.pick({
+  name: true,
+  email: true,
+});
+
+export type UserRegister = z.infer<typeof UserSchema>;
+export type UserLogin = z.infer<typeof LoginUserSchema>;
