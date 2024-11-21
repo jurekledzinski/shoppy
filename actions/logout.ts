da844ec1c897e5bead0cfd3c9a714c11bd22609a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-21 17:04:07 +0100

    Add logout action

---
+++ actions/logout.ts
@@ -0,0 +1,17 @@
+import { actionTryCatch } from '@/helpers';
+
+export const logout = actionTryCatch(async () => {
+  const res = await fetch('api/v1/logout', {
+    body: JSON.stringify({}),
+    method: 'POST',
+    cache: 'no-store',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'same-origin',
+  });
+
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+
+  return { message: 'Logout successful', success: true };
+});
