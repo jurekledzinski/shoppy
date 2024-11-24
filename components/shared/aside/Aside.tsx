c963878e8078f7fc24d2e953f18813543d57ea84
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-24 20:00:32 +0100

    Update aside component

--- components/shared/aside/Aside.tsx
+++ components/shared/aside/Aside.tsx
@@ -1,20 +1,33 @@
 'use client';
 import styles from './Aside.module.css';
 import { Cart } from '../cart';
-import { ContactForm, ForgetPasswordForm } from '@/components/pages';
+import { contact, login, logout, register, resetPassword } from '@/actions';
 import { controlAside } from '@/helpers';
-import { login, logout, register } from '@/actions';
-import { LoginPanel, MenuPanel, RegisterPanel } from '@/components/pages';
-import { setDefaultCloseAside } from './helpers';
+import { forgetPassword } from '@/actions';
+import { showToast } from '@helpers/toasts';
 import { useActionState } from 'react';
 import { useAside } from '@/store/aside';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useUser } from '@/store/user';
+
+import {
+  ContactPanel,
+  ForgetPasswordPanel,
+  MenuPanel,
+  LoginPanel,
+  RegisterPanel,
+  ResetPasswordPanel,
+} from '@/components/pages';
 
 import {
   useActionStateAndReset,
   useLoadUser,
   useLoginForm,
   useRegisterForm,
+  useResetPasswordForm,
+  useForgetPasswordForm,
+  useLoadResetPasswordForm,
+  useContactForm,
 } from '@/hooks';
 
 export const Aside = () => {
@@ -24,6 +37,9 @@ export const Aside = () => {
   const stateOpen = context.value;
   const userId = user.payload?.id ?? '';
   const userName = user.payload?.name ?? '';
+  const searchParams = useSearchParams();
+  const paramActionType = searchParams.get('action_type');
+  const router = useRouter();
 
   const { resetStateAction: resetStateActionLogout } = useActionStateAndReset({
     fnAction: logout,
@@ -45,23 +61,85 @@ export const Aside = () => {
     }
   );
 
+  const [stateResetPassword, formActionResetPassword, isPendingResetPassword] =
+    useActionState(resetPassword, {
+      message: '',
+      success: false,
+    });
+
+  const [stateContact, formActionContact, isPendingContact] = useActionState(
+    contact,
+    {
+      message: '',
+      success: false,
+    }
+  );
+
+  const { methodsContact, onSubmitContact } = useContactForm({
+    formAction: formActionContact,
+    isPending: isPendingContact,
+    isSuccess: stateContact.success,
+    onSuccessAction: () => {
+      showToast(stateContact.message);
+      context.onChange(actionElement, false);
+    },
+  });
+
+  const [
+    stateForgetPassword,
+    formActionForgetPassword,
+    isPendingForgetPassword,
+  ] = useActionState(forgetPassword, {
+    message: '',
+    success: false,
+  });
+
+  const { methodsForgetPassword, onSubmitForgetPassword } =
+    useForgetPasswordForm({
+      formAction: formActionForgetPassword,
+      isPending: isPendingForgetPassword,
+      isSuccess: stateForgetPassword.success,
+      onSuccessAction: () => {
+        showToast(stateForgetPassword.message, 10000);
+        context.onChange(actionElement, false);
+      },
+    });
+
   const { methodsLogin, onSubmitLogin } = useLoginForm({
     formAction: actionLogin.formAction,
     isPending: actionLogin.isPending,
     isSuccess: actionLogin.state.success,
-    onSuccessAction: () =>
-      setDefaultCloseAside(context, 'Login successful', actionElement),
+    onSuccessAction: () => {
+      showToast('Login successful');
+      context.onChange(actionElement, false);
+    },
   });
 
   const { methodsRegister, onSubmitRegister } = useRegisterForm({
     formAction: formActionRegister,
     isPending: isPendingRegister,
     isSuccess: stateRegister.success,
-    onSuccessAction: () =>
-      setDefaultCloseAside(context, 'Register successful', actionElement),
+    onSuccessAction: () => {
+      showToast('Register successful');
+      context.onChange(actionElement, false);
+    },
+  });
+
+  const { methodsResetPassword, onSubmitResetPassword } = useResetPasswordForm({
+    formAction: formActionResetPassword,
+    isPending: isPendingResetPassword,
+    isSuccess: stateResetPassword.success,
+    onSuccessAction: () => {
+      router.replace(window.location.pathname);
+      showToast('Reset password successful');
+      setTimeout(() => {
+        controlAside(context, 'login', actionElement, stateOpen);
+      }, 500);
+    },
   });
 
   useLoadUser({ body: actionLogin.state.body, onChange: user.onChange });
+  useLoadResetPasswordForm({ context, paramActionType });
 
   return (
     <aside
@@ -71,13 +149,14 @@ export const Aside = () => {
     >
       {context.type === 'menu' ? (
         <MenuPanel
-          onCloseAside={() => {
+          onCloseAsideAction={() => {
             context.onChange(actionElement, false);
           }}
-          onLogout={() => {
+          onLogoutAction={() => {
             resetStateActionLogin();
             resetStateActionLogout(new FormData());
-            setDefaultCloseAside(context, 'Logout successful', actionElement);
+            showToast('Logout successful');
+            context.onChange(actionElement, false);
           }}
           onRedirectContact={() => {
             controlAside(context, 'contact', actionElement, stateOpen);
@@ -96,13 +175,14 @@ export const Aside = () => {
           <Cart />
         </>
       ) : context.type === 'contact' ? (
-        <>
-          <header className={styles.header}>Contact</header>
-          <ContactForm />
-        </>
+        <ContactPanel
+          isPending={isPendingContact}
+          methods={methodsContact}
+          onSubmitAction={onSubmitContact}
+          state={stateContact}
+        />
       ) : context.type === 'login' ? (
         <LoginPanel
-          context={context}
           isPending={actionLogin.isPending}
           methods={methodsLogin}
           onSubmitAction={onSubmitLogin}
@@ -118,7 +198,6 @@ export const Aside = () => {
         />
       ) : context.type === 'register' ? (
         <RegisterPanel
-          context={context}
           isPending={isPendingRegister}
           methods={methodsRegister}
           onSubmitAction={onSubmitRegister}
@@ -129,9 +208,27 @@ export const Aside = () => {
           }}
         />
       ) : context.type === 'forget-password' ? (
+        <ForgetPasswordPanel
+          isPending={isPendingForgetPassword}
+          methods={methodsForgetPassword}
+          onSubmitAction={onSubmitForgetPassword}
+          state={stateForgetPassword}
+        />
+      ) : context.type === 'reset_password' ? (
         <>
-          <header className={styles.header}>Forget password</header>
-          <ForgetPasswordForm />
+          <ResetPasswordPanel
+            isPending={isPendingResetPassword}
+            methods={methodsResetPassword}
+            onCancelAction={(e) => {
+              e.preventDefault();
+              router.replace(window.location.pathname);
+              setTimeout(() => {
+                context.onChange(actionElement, false);
+              }, 500);
+            }}
+            onSubmitAction={onSubmitResetPassword}
+            state={stateResetPassword}
+          />
         </>
       ) : null}
     </aside>
