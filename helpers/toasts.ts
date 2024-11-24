f2783ebbf200cf1728133c47c2b84c5b08a95f53
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-24 20:01:14 +0100

    Modify helpers set default close aside to show toast

---
+++ helpers/toasts.ts
@@ -0,0 +1,9 @@
+import { toast } from 'react-toastify';
+
+export const showToast = (message: string, autoClose?: number) => {
+  const theme = JSON.parse(localStorage.getItem('mode')!) || 'light';
+  toast.success(message, { theme, ...(autoClose && { autoClose }) });
+
+  const check = { theme, ...(autoClose && { autoClose }) };
+  console.log('check', check);
+};
