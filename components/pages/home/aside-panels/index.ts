5f4763e37c14aec2f8e77c38ba206b3b7623dbdb
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-24 19:59:48 +0100

    Update exports in aside panel

--- components/pages/home/aside-panels/index.ts
+++ components/pages/home/aside-panels/index.ts
@@ -1,3 +1,6 @@
 export * from './login-panel';
 export * from './menu-panel';
 export * from './register-panel';
+export * from './forget-password-panel';
+export * from './reset-password-panel';
+export * from './contact-panel';
