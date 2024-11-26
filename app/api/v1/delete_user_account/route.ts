3f953bc5290a141930598207629ca59a2af9b4d1
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-26 11:17:38 +0100

    Create api route for delete user account

--- app/api/v1/delete_user_account/route.ts
+++ app/api/v1/delete_user_account/route.ts
@@ -1,1 +1,33 @@
-// sprawdź cookie auth  jeśli nie ma lub invalidate token to wtedy response error
+import 'server-only';
+import { NextRequest, NextResponse } from 'next/server';
+import { connectDB, getCollectionDb, verifyToken } from '@/lib';
+import { UserID, UserRegister } from '@/models';
+import { errorMessage } from '@/helpers';
+import { ObjectId } from 'mongodb';
+
+export const DELETE = connectDB(async (request: NextRequest) => {
+  const secret = process.env.JWT_SECRET_ACCESS!;
+  const cookie = request.cookies.get('auth');
+
+  if (!cookie) return errorMessage(401);
+
+  const decoded = verifyToken(cookie.value, secret) as UserID;
+
+  const collection = getCollectionDb<Omit<UserRegister, '_id'>>('users');
+
+  if (!collection) return errorMessage(500);
+
+  const result = await collection.deleteOne({ _id: new ObjectId(decoded._id) });
+
+  console.log('result delete api', result);
+
+  console.log('cookie api delete user', cookie);
+
+  const response = NextResponse.json({
+    success: true,
+  });
+
+  response.cookies.delete('auth');
+
+  return response;
+});
