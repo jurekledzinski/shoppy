7868a6228bf6672a5133d1d8faf3bea97cce788f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-26 11:19:01 +0100

    Create api route delete user account

--- app/api/v1/update_user_profile/route.ts
+++ app/api/v1/update_user_profile/route.ts
@@ -1,1 +1,30 @@
-// sprawdź cookie auth  jeśli nie ma lub invalidate token to wtedy response error
+import 'server-only';
+import { NextRequest } from 'next/server';
+import { connectDB, getCollectionDb, verifyToken } from '@/lib';
+import { errorMessage } from '@/helpers';
+import { UserID, UserRegister, UserUpdateProfile } from '@/models';
+import { ObjectId } from 'mongodb';
+
+export const PATCH = connectDB(async (request: NextRequest) => {
+  const secret = process.env.JWT_SECRET_ACCESS!;
+  const cookie = request.cookies.get('auth');
+  const body = (await request.json()) as UserUpdateProfile;
+
+  if (!cookie) return errorMessage(401);
+
+  const decoded = verifyToken(cookie.value, secret) as UserID;
+
+  const collection = getCollectionDb<Omit<UserRegister, '_id'>>('users');
+
+  if (!collection) return errorMessage(500);
+
+  const result = await collection.updateOne(
+    { _id: new ObjectId(decoded._id) },
+    { $set: { email: body.email, name: body.name } }
+  );
+
+  console.log('result', result);
+
+  console.log('cookie api update profile', cookie);
+  return Response.json({ success: true });
+});
