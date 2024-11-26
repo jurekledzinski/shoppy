98db94d53a4f345fe85096648496c9e15b979cb5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-26 11:23:30 +0100

    Update exports in hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -7,3 +7,5 @@ export * from './useActionStateAndReset';
 export * from './useLoadUser';
 export * from './useResetPasswordForm';
 export * from './useLoadResetPasswordForm';
+export * from './useUpdateProfileForm';
+export * from './useChangePasswordForm';
