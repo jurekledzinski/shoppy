0baa9228bd05e56c4c66c360bce01b8d4bbdc2d0
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-26 11:19:30 +0100

    Add fetch data user in server component profile

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,5 +1,33 @@
-const Profile = () => {
-  return <div>Profile page</div>;
+import { ProfileSection } from '@/components/pages';
+import { UserRegister } from '@/models';
+import { cookies, headers } from 'next/headers';
+
+type Params = Promise<{ id: string }>;
+
+const Profile = async (props: { params: Params }) => {
+  const headersList = await headers();
+  const host = headersList.get('host');
+  const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
+  const cookieStore = await cookies();
+  const userSession = cookieStore.get('auth');
+  const params = await props.params;
+  const url = `${protocol}://${host}/api/v1/get_user?id=${params.id}`;
+  const response = await fetch(url, {
+    cache: 'force-cache',
+    headers: {
+      Cookie: `auth=${userSession?.value}`,
+    },
+    next: { tags: ['update-profile'] },
+  });
+
+  const userData = (await response.json()) as {
+    payload: Omit<UserRegister, 'password'>;
+    success: boolean;
+  };
+
+  console.log('userData page profile', userData);
+
+  return <ProfileSection user={response.ok ? userData.payload : null} />;
 };
 
 export default Profile;
