b9578eb874fe63be8163b8bc99f3452e168ad13b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-29 14:33:12 +0100

    Add slider to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import styles from '@styles/HomePage.module.css';
+import { BasicSlider } from '@/components/shared';
 
 export default function Home() {
   return (
     <div className={styles.home}>
-      <div className={styles.slider}>Slider</div>
+      <div className={styles.slider}>
+        <BasicSlider />
+      </div>
       <div className={styles.brands}>Brands</div>
     </div>
   );
