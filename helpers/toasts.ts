b8dff2423d93ffedb5b9a164d5dad823ab1cfa01
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-29 14:39:12 +0100

    Remove unnecessary code

--- helpers/toasts.ts
+++ helpers/toasts.ts
@@ -3,7 +3,4 @@ import { toast } from 'react-toastify';
 export const showToast = (message: string, autoClose?: number) => {
   const theme = JSON.parse(localStorage.getItem('mode')!) || 'light';
   toast.success(message, { theme, ...(autoClose && { autoClose }) });
-
-  const check = { theme, ...(autoClose && { autoClose }) };
-  console.log('check', check);
 };
