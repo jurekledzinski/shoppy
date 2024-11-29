48c9c79d5546cd8d66a4a4c12b4ef2644cfc44f7
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-11-29 14:31:04 +0100

    Add remote image patterns in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+        pathname: '/mdklwracd5rti/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
