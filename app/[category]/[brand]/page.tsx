421faf294b1de83b0b19eed0981fd036de5b651c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-02 12:32:10 +0100

    Add fetch brands

--- app/[category]/[brand]/page.tsx
+++ app/[category]/[brand]/page.tsx
@@ -1,5 +1,28 @@
-const Products = () => {
-  return <div>Products</div>;
+import { headers } from 'next/headers';
+import { ProductsSection } from '@/components/pages';
+
+type Params = Promise<{ category: string; brand: string }>;
+
+const getDomain = async () => {
+  const headersList = await headers();
+  const host = headersList.get('host');
+  const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
+  return `${protocol}://${host}`;
+};
+
+const fetchProducts = async (url: string) => {
+  const response = await fetch(url, { cache: 'force-cache' });
+  return response;
+};
+
+const Products = async (props: { params: Params }) => {
+  const domain = await getDomain();
+  const params = await props.params;
+  const urlGetProducts = `${domain}/api/v1/products?category=${params.category}&brand=${params.brand}`;
+  const response = await fetchProducts(urlGetProducts);
+  const products = await response.json();
+
+  return <ProductsSection data={response.ok ? products.payload : []} />;
 };
 
 export default Products;
