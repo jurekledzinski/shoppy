e686d3934bd3366587a949d0d937ea5446876e74
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-02 12:33:11 +0100

    Add api for fetch products

---
+++ app/api/v1/products/route.ts
@@ -0,0 +1,30 @@
+import 'server-only';
+import { connectDB, getCollectionDb } from '@/lib';
+import { errorMessage } from '@/helpers';
+import { NextRequest } from 'next/server';
+import { ProductCard } from '@/models';
+
+export const GET = connectDB(async (request: NextRequest) => {
+  const { searchParams } = new URL(request.url);
+  const category = searchParams.get('category');
+  const brand = searchParams.get('brand');
+
+  console.log(category, brand);
+
+  if (!category || category === 'undefined') return errorMessage(404);
+  if (!brand || brand === 'undefined') return errorMessage(404);
+
+  const collection = getCollectionDb<ProductCard>('products');
+
+  if (!collection) return errorMessage(500);
+
+  const products = await collection
+    .find<ProductCard>({ category, brand })
+    .toArray();
+
+  console.log('check', products);
+
+  if (!products) return errorMessage(404);
+
+  return Response.json({ success: true, payload: products });
+});
