fd0b4e9c3d0b1cd5643391a1533a41e29f31ac0e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-03 16:39:20 +0100

    Create cart quantity controller component for do client actions

---
+++ components/pages/details-product/cart-quantity-contoller/CartQuantityContoller.tsx
@@ -0,0 +1,36 @@
+'use client';
+import React, { useState } from 'react';
+import { CartQuantityContollerProps } from './types';
+import { AddToCartButton, CartItemCounter } from '@/components/shared';
+
+export const CartQuantityContoller = ({ data }: CartQuantityContollerProps) => {
+  const [quanity, setQuantity] = useState(1);
+
+  const addLocalQuantity = () => {
+    setQuantity((prev) => prev + 1);
+  };
+  const subtractLocalQuantity = () => {
+    setQuantity((prev) => prev - 1);
+  };
+
+  const addGlobalQuantity = () => {};
+  const subtractGlobalQuantity = () => {};
+
+  return (
+    <React.Fragment>
+      <CartItemCounter
+        addGlobalQuantity={addGlobalQuantity}
+        addLocalQuantity={addLocalQuantity}
+        subtractGlobalQuantity={subtractGlobalQuantity}
+        subtractLocalQuantity={subtractLocalQuantity}
+        quanity={quanity}
+        onStock={data.onStock}
+      />
+      <AddToCartButton
+        onClick={() => {
+          console.log('Confirm add to cart quantity', quanity);
+        }}
+      />
+    </React.Fragment>
+  );
+};
---
+++ components/pages/details-product/cart-quantity-contoller/types.ts
@@ -0,0 +1,5 @@
+import { ProductCart } from '@/models';
+
+export type CartQuantityContollerProps = {
+  data: ProductCart;
+};
