e5242db8443e6c1097adb8414e99277311d3286e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-04 22:26:11 +0100

    Update add to cart button component

--- components/shared/add-to-cart-button/AddToCartButton.tsx
+++ components/shared/add-to-cart-button/AddToCartButton.tsx
@@ -1,11 +1,14 @@
 'use client';
+import styles from '@styles/buttons.module.css';
 import { AddToCartButtonProps } from './types';
 import { Button } from '@/components/shared';
 
-import styles from './AddToCartButton.module.css';
-
 export const AddToCartButton = ({ onClick }: AddToCartButtonProps) => {
   return (
-    <Button className={styles.button} onClick={onClick} text="Add to cart" />
+    <Button
+      className={styles.buttonAddToCart}
+      onClick={onClick}
+      text="Add to cart"
+    />
   );
 };
