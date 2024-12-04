d9f8edd798a9f2f7562bc9812edc531b5abbd745
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-04 22:19:55 +0100

    Update cart component

--- components/shared/cart/Cart.tsx
+++ components/shared/cart/Cart.tsx
@@ -1,6 +1,8 @@
 'use client';
 import styles from './Cart.module.css';
+import { CartProps } from './types';
 
-export const Cart = () => {
+export const Cart = ({ data }: CartProps) => {
+  console.log('Cart data', data);
   return <div className={styles.cart}>Cart</div>;
 };
