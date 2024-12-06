1b90701602a2eaeb84ce72c7d2fc8985ea785e89
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-06 15:28:49 +0100

    Add disabled property to add to cart button

--- components/shared/add-to-cart-button/AddToCartButton.tsx
+++ components/shared/add-to-cart-button/AddToCartButton.tsx
@@ -3,12 +3,16 @@ import styles from '@styles/buttons.module.css';
 import { AddToCartButtonProps } from './types';
 import { Button } from '@/components/shared';
 
-export const AddToCartButton = ({ onClick }: AddToCartButtonProps) => {
+export const AddToCartButton = ({
+  disabled,
+  onClick,
+}: AddToCartButtonProps) => {
   return (
     <Button
       className={styles.buttonAddToCart}
       onClick={onClick}
       text="Add to cart"
+      disabled={disabled}
     />
   );
 };
