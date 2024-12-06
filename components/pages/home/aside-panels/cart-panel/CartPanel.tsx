32ef52487f4bc7599f78afe0d0a9788cd086a61a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-06 12:04:12 +0100

    Create folder and files for CartPanel

---
+++ components/pages/home/aside-panels/cart-panel/CartPanel.tsx
@@ -0,0 +1,3 @@
+export const CartPanel = () => {
+  return <div></div>;
+};
--- components/pages/home/aside-panels/index.ts
+++ components/pages/home/aside-panels/index.ts
@@ -4,3 +4,4 @@ export * from './register-panel';
 export * from './forget-password-panel';
 export * from './reset-password-panel';
 export * from './contact-panel';
+export * from './cart-panel';
