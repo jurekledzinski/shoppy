7cb6225dbdc7a1333fc3996a029ed453b70b0ad9
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-11 11:16:18 +0100

    Update middleware for next auth

--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,21 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { jwtVerify } from 'jose';
 
-export async function middleware(request: NextRequest) {
+import { auth } from '@/auth';
+
+export default auth((request) => {
   const { pathname } = request.nextUrl;
-  const token = request.cookies.get('auth');
 
-  //   if (!token) {
-  //     const url = request.nextUrl.clone();
-  //     url.pathname = '/';
-  //     return NextResponse.redirect(url);
-  //   }
-
-  try {
-    if (token) {
-      const secretKey = new TextEncoder().encode(
-        process.env.JWT_SECRET_ACCESS!
-      );
-
-      await jwtVerify(token.value, secretKey);
-    }
-
-    if (
-      (pathname.startsWith('/profile') && !token) ||
-      (pathname.startsWith('/orders') && !token)
-    ) {
-      const url = request.nextUrl.clone();
-      url.pathname = '/';
-      return NextResponse.redirect(url);
-    }
-
-    return NextResponse.next();
-  } catch (err) {
-    console.log('ee', err);
-    const url = request.nextUrl.clone();
-    url.pathname = '/';
-    return NextResponse.redirect(url);
+  if (
+    (!request.auth && pathname.startsWith('/profile')) ||
+    (!request.auth && pathname.startsWith('/orders'))
+  ) {
+    const newUrl = new URL('/', request.nextUrl.origin);
+    return NextResponse.redirect(newUrl);
   }
-}
+
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: ['/profile/:path*', '/orders/:path*'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
