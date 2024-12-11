ff8aa0388184488412f4021de00dcfb7e5461859
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-11 22:04:33 +0100

    Add message for wrong credentials in next auth login

--- helpers/messagesJwtWebToken.ts
+++ helpers/messagesJwtWebToken.ts
@@ -4,6 +4,8 @@ export const transformMessage = (name: string) => {
       return 'Invalid or expired link, please try one more time';
     case 'JsonWebTokenError':
       return 'Invalid or expired link, please try one more time';
+    case 'CredentialsSignin':
+      return 'Incorrect credentials';
     default:
       return 'Something went wrong, please one more time';
   }
