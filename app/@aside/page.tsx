78b97586dd8014279bdd7efee39a2e47b8ce2b73
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-11 11:43:07 +0100

    Pass session data user to aside component

--- app/@aside/page.tsx
+++ app/@aside/page.tsx
@@ -1,7 +1,10 @@
 import { Aside as MainAside } from '@/components/shared';
+import { auth } from '@/auth';
 
-const Aside = () => {
-  return <MainAside />;
+const Aside = async () => {
+  const session = await auth();
+
+  return <MainAside userData={session} />;
 };
 
 export default Aside;
