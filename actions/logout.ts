f1d9bcd1483ceb063a9eb67245842124ab2f0226
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-11 11:42:29 +0100

    Change action logout for next auth

--- actions/logout.ts
+++ actions/logout.ts
@@ -1,18 +1,10 @@
 'use client';
-import { redirect } from 'next/navigation';
+
 import { actionTryCatch } from '@/helpers';
 
+import { logOut } from '@/auth';
+
 export const logout = actionTryCatch(async () => {
-  const res = await fetch('/api/v1/logout', {
-    body: JSON.stringify({}),
-    method: 'POST',
-    cache: 'no-store',
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  if (!res.ok) {
-    throw new Error(res.statusText);
-  }
-
-  redirect('/');
+  await logOut();
+  return { message: 'Logout success', success: true };
 });
