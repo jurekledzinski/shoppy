e5a6136198aae3bb0489b91a31002dc5dafba7e6
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-11 13:13:23 +0100

    Remove helper function to create token

--- lib/createToken.ts
+++
@@ -1,8 +0,0 @@
-import 'server-only';
-import jwt from 'jsonwebtoken';
-
-export const createToken = (_id: string) => {
-  const secret = process.env.JWT_SECRET_ACCESS!;
-  const lifeTimeAccessToken = process.env.JWT_LIFETIME_ACCESS!;
-  return jwt.sign({ _id }, secret, { expiresIn: lifeTimeAccessToken });
-};
