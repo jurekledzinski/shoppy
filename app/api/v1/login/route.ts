c7556001321197ef01ff3df383bb214f4067fef3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-11 11:13:26 +0100

    Change login api route to work with next auth login

--- app/api/v1/login/route.ts
+++ app/api/v1/login/route.ts
@@ -2,41 +2,26 @@ import 'server-only';
 import { errorMessage } from '@/helpers';
 import { UserLogin, UserRegister } from '@/models';
 import { NextRequest, NextResponse } from 'next/server';
-import {
-  getCollectionDb,
-  connectDB,
-  comparePasswords,
-  createToken,
-} from '@/lib';
+import { getCollectionDb, connectDB, comparePasswords } from '@/lib';
 
 export const POST = connectDB(async (req: NextRequest) => {
   const body = (await req.json()) as UserLogin;
 
-  const collection = getCollectionDb<UserRegister>('users');
+  const collection = getCollectionDb<Omit<UserRegister, '_id'>>('users');
 
   if (!collection) return errorMessage(500);
 
-  const user = await collection.findOne({ email: body.email });
+  const user = await collection.findOne<UserRegister>({ email: body.email });
 
-  if (!user) return errorMessage(409, 'User not found');
+  if (!user) return errorMessage(409, 'User not exist');
 
   const isMatch = await comparePasswords(body.password, user.password);
 
   if (!isMatch) return errorMessage(409, 'Incorrect credentials');
 
-  const token = createToken(user._id);
-
   const response = NextResponse.json({
     success: true,
-    payload: { email: user.email, id: user._id, name: user.name },
-  });
-
-  response.cookies.set('auth', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 3600,
-    path: '/',
-    sameSite: 'strict',
+    payload: { name: user.name, email: user.email, id: user._id },
   });
 
   return response;
