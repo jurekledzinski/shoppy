b5e45adcd48957bda9808df1d5da5c98289987a7
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-13 16:22:54 +0100

    Update review action

--- actions/review.ts
+++ actions/review.ts
@@ -1,30 +1,64 @@
 'use server';
-import { actionTryCatch } from '@/helpers';
-import { ReviewSchema } from '@/models';
-import { revalidateTag } from 'next/cache';
+import { connectDBAction, getCollectionDb } from '@/lib';
+import { errorMessageAction, roundAvergeRate } from '@/helpers';
+import { getToken } from 'next-auth/jwt';
 import { headers } from 'next/headers';
-import { convertHeadersToObject } from '@/helpers';
+import { ObjectId } from 'mongodb';
+import { Product, Review, ReviewSchema } from '@/models';
+import { revalidateTag } from 'next/cache';
 
-export const review = actionTryCatch(
+const secret = process.env.AUTH_SECRET;
+
+export const review = connectDBAction(
   async (prevState: unknown, formData: FormData) => {
+    const reviewHeaders = await headers();
     const body = Object.fromEntries(formData);
     const fomattedBody = { ...body, rate: parseFloat(body.rate.toString()) };
 
-    const parsedData = ReviewSchema.parse(fomattedBody);
+    const parsedData = ReviewSchema.parse(fomattedBody) as Omit<Review, '_id'>;
 
-    const headersReview = await headers();
-    const header = await convertHeadersToObject(headersReview);
+    const token = await getToken({ req: { headers: reviewHeaders }, secret });
 
-    const res = await fetch('http://localhost:3000/api/v1/review', {
-      body: JSON.stringify(parsedData),
-      method: 'POST',
-      cache: 'no-store',
-      headers: { ...header, 'Content-Type': 'application/json' },
-    });
+    if (!token) return errorMessageAction('Unauthorized');
 
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
+    const collectionReview = getCollectionDb<Omit<Review, '_id'>>('reviews');
+
+    if (!collectionReview) return errorMessageAction('Internal server error');
+
+    await collectionReview.insertOne({ ...parsedData });
+
+    const reviewProductId = parsedData.productId;
+
+    const productSumRate = await collectionReview
+      .aggregate([
+        {
+          $match: { productId: reviewProductId },
+        },
+        {
+          $group: {
+            _id: '$productId',
+            totalRate: { $sum: '$rate' },
+            amount: { $sum: 1 },
+          },
+        },
+      ])
+      .toArray();
+
+    const averageRate = productSumRate[0].totalRate / productSumRate[0].amount;
+
+    const collectionProducts =
+      getCollectionDb<Omit<Product, '_id'>>('products');
+
+    if (!collectionProducts) return errorMessageAction('Internal server error');
+
+    const newRate = roundAvergeRate(averageRate);
+
+    await collectionProducts.updateOne(
+      {
+        _id: new ObjectId(reviewProductId),
+      },
+      { $set: { rate: newRate } }
+    );
 
     revalidateTag('get_product_reviews');
     revalidateTag('get_product');
