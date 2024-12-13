08dc12f98fedfa0761a4676aae10a3094603ba63
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-13 16:48:55 +0100

    Update register action

--- actions/register.ts
+++ actions/register.ts
@@ -1,22 +1,29 @@
-import { UserSchema } from '@/models';
-import { actionTryCatch } from '@/helpers';
+'use server';
+import bcrypt from 'bcrypt';
+import { connectDBAction, getCollectionDb } from '@/lib';
+import { errorMessageAction } from '@/helpers';
+import { UserRegister, UserSchema } from '@/models';
 
-export const register = actionTryCatch(
+export const register = connectDBAction(
   async (prevState: unknown, formData: FormData) => {
     const body = Object.fromEntries(formData);
 
     const parsedData = UserSchema.parse(body);
 
-    const res = await fetch('/api/v1/register', {
-      body: JSON.stringify(parsedData),
-      method: 'POST',
-      cache: 'no-store',
-      headers: { 'Content-Type': 'application/json' },
+    const collection = getCollectionDb<UserRegister>('users');
+
+    if (!collection) return errorMessageAction('Internal server error');
+
+    const user = await collection.findOne<UserRegister>({
+      email: parsedData.email,
     });
 
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
+    if (user) return errorMessageAction('Email already in use');
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(parsedData.password, salt);
+
+    await collection.insertOne({ ...parsedData, password: hash });
 
     return { message: 'Register successful', success: true };
   }
