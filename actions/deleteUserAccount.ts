2984a42749b6088d19503a344a9bf27bf79f444e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-13 16:20:07 +0100

    Update delete user  action

--- actions/deleteUserAccount.ts
+++ actions/deleteUserAccount.ts
@@ -1,19 +1,30 @@
+'use server';
 import { actionTryCatch } from '@/helpers';
+import { revalidateTag } from 'next/cache';
+import { headers } from 'next/headers';
+import { convertHeadersToObject } from '@/helpers';
+import { getDomain } from '@/app/_helpers';
 
 export const deleteUserAccount = actionTryCatch(
   async (prevState: unknown, formData: FormData) => {
     const body = Object.fromEntries(formData);
+    const domain = await getDomain();
 
-    const res = await fetch('/api/v1/delete_user_account', {
+    const headersUser = await headers();
+    const header = await convertHeadersToObject(headersUser);
+    const url = `${domain}/api/v1/delete_user_account`;
+
+    const res = await fetch(url, {
       body: JSON.stringify(body),
       method: 'DELETE',
-      cache: 'no-store',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { ...header, 'Content-Type': 'application/json' },
     });
 
     if (!res.ok) {
       throw new Error(res.statusText);
     }
+
+    revalidateTag('get_user');
 
     return { message: 'Delete user successful', success: true };
   }
