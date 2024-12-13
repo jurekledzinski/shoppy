0792f9b4071875b240f64a67b631bdd3b5ce82e5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-13 17:20:19 +0100

    Update change password action

--- actions/changeUserPassword.ts
+++ actions/changeUserPassword.ts
@@ -1,30 +1,44 @@
 'use server';
-import { PasswordSchema } from '@/models';
-import { actionTryCatch } from '@/helpers';
+import bcrypt from 'bcrypt';
+import { connectDBAction, getCollectionDb } from '@/lib';
+import { errorMessageAction } from '@/helpers';
+import { getToken } from 'next-auth/jwt';
 import { headers } from 'next/headers';
-import { convertHeadersToObject } from '@/helpers';
-import { getDomain } from '@/app/_helpers';
+import { ObjectId } from 'mongodb';
+import { PasswordSchema, UserRegister } from '@/models';
 
-export const changeUserPassword = actionTryCatch(
+const secret = process.env.AUTH_SECRET;
+
+export const changeUserPassword = connectDBAction(
   async (prevState: unknown, formData: FormData) => {
+    const userHeaders = await headers();
     const body = Object.fromEntries(formData);
-    const domain = await getDomain();
 
     const parsedData = PasswordSchema.parse(body);
 
-    const headersPassword = await headers();
-    const header = await convertHeadersToObject(headersPassword);
-    const url = `${domain}/api/v1/change_user_password`;
+    const token = await getToken({ req: { headers: userHeaders }, secret });
 
-    const res = await fetch(url, {
-      body: JSON.stringify(parsedData),
-      method: 'PATCH',
-      headers: { ...header, 'Content-Type': 'application/json' },
+    if (!token) return errorMessageAction('Unauthorized');
+
+    const userId = token.id as string;
+
+    const collection = getCollectionDb<Omit<UserRegister, '_id'>>('users');
+
+    if (!collection) return errorMessageAction('Internal server error');
+
+    const user = await collection.findOne<UserRegister>({
+      _id: new ObjectId(userId),
     });
 
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
+    if (!user) return errorMessageAction('Incorrect credentials');
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(parsedData.password, salt);
+
+    await collection.updateOne(
+      { _id: new ObjectId(userId) },
+      { $set: { password: hash } }
+    );
 
     return { message: 'Password change successful', success: true };
   }
