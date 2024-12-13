c09fced8c4ac98ef361fbf8c9ef1991b23600655
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-13 16:21:35 +0100

    Update reset password action

--- actions/resetPassword.ts
+++ actions/resetPassword.ts
@@ -1,26 +1,45 @@
-'use client';
-import { actionTryCatch } from '@/helpers';
-import { PasswordSchema, UserResetPassword } from '@/models';
+'use server';
+import bcrypt from 'bcrypt';
+import { connectDBAction, getCollectionDb, verifyToken } from '@/lib';
+import { errorMessageAction } from '@/helpers';
 
-export const resetPassword = actionTryCatch(
+import {
+  PasswordSchema,
+  UserResetPassword,
+  UserForgetPassword,
+} from '@/models';
+
+const secret = process.env.JWT_SECRET_FORGET_PASSWORD!;
+
+export const resetPassword = connectDBAction(
   async (prevState: unknown, formData: FormData) => {
     const body = Object.fromEntries(formData) as UserResetPassword;
-
-    PasswordSchema.parse(body);
-
-    const apiUrl = `/api/v1/reset_password/?token=${body.token}`;
+    const resetToken = body.token;
     delete body.token;
 
-    const res = await fetch(apiUrl, {
-      body: JSON.stringify(body),
-      method: 'POST',
-      cache: 'no-store',
-      headers: { 'Content-Type': 'application/json' },
+    const parsedData = PasswordSchema.parse(body);
+
+    if (!resetToken) return errorMessageAction('Unauthorized');
+
+    const decoded = verifyToken(resetToken, secret) as UserForgetPassword;
+
+    const collection = getCollectionDb<UserResetPassword>('users');
+
+    if (!collection) return errorMessageAction('Internal server error');
+
+    const user = await collection.findOne<UserResetPassword>({
+      email: decoded.email,
     });
 
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
+    if (!user) return errorMessageAction('Incorrect credentials');
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(parsedData.password, salt);
+
+    await collection.updateOne(
+      { email: decoded.email },
+      { $set: { password: hash } }
+    );
 
     return {
       message: 'Password reset successfully',
