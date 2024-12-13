b0902e2240723cd173c81e760539788f6c14c4c8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-13 16:31:19 +0100

    Add logs to see session in aside

--- app/@aside/default.tsx
+++ app/@aside/default.tsx
@@ -4,6 +4,8 @@ import { auth } from '@/auth';
 const Aside = async () => {
   const session = await auth();
 
+  console.log('aside session default', session);
+
   return <MainAside userData={session} />;
 };
 
