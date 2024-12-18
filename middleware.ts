70ed86877251593c941574db74e5ea39fde50256
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-18 12:25:14 +0100

    Work on secure pages for checkout and prevent to click in stepper forward

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 
 import { auth } from '@/auth';
 
+const checkoutProcess = ['/shipping', '/place-order', '/details-order'];
+
 export default auth((request) => {
   const { pathname } = request.nextUrl;
 
@@ -10,6 +12,53 @@ export default auth((request) => {
     (!request.auth && pathname.startsWith('/orders'))
   ) {
     const newUrl = new URL('/', request.nextUrl.origin);
+    return NextResponse.redirect(newUrl);
+  }
+
+  console.log('pathname middleware', pathname);
+
+  const protecedCheckout = checkoutProcess.some((item) =>
+    pathname.includes(item)
+  );
+
+  // weryfikacja cookie value
+  // zdekodowana wartość path
+  const przykladPathWcookie = '/shipping/place-order';
+
+  //Kiedy nie zalogowany user lub gdt nie ma cookie lub token jest uszkodzony tez dodajitp
+
+  if (
+    (!request.auth && protecedCheckout) ||
+    (!przykladPathWcookie && protecedCheckout)
+  ) {
+    const newUrl = new URL('/', request.nextUrl.origin);
+    return NextResponse.redirect(newUrl);
+  }
+
+  //Kiedy zalogowany user lub quest session to protect dalsze kroki jesli bedzie chcial przeskoczyc kroki
+
+  //   same checkoutProcess musi chronic dostep do stron bez cookie stepper
+
+  //   pathname will be stored in cookie in this case
+  //   cookie controll access and session user or session guest user
+  //   tu pathname z cookie
+
+  const restProtectedSteps = checkoutProcess.filter(
+    (item) => !przykladPathWcookie.includes(item)
+  );
+
+  const isNotAllowedStep = restProtectedSteps.some((item) =>
+    pathname.includes(item)
+  );
+
+  console.log('restProtectedSteps -----', restProtectedSteps);
+
+  console.log('isNotAllowedStep', isNotAllowedStep);
+
+  //   lepiej nie polegac na cookie tylko znalezc gdzie redirect
+
+  if (request.auth && isNotAllowedStep) {
+    const newUrl = new URL(przykladPathWcookie, request.nextUrl.origin);
     return NextResponse.redirect(newUrl);
   }
 
