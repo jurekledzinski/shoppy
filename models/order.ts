ce6f417f4cdd59be16b03a860560627d24628abe
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-22 17:47:49 +0100

    Update in order model schema

--- models/order.ts
+++ models/order.ts
@@ -8,6 +8,12 @@ export const OrderSchema = z.object({
   postCode: z.string({ required_error: 'Post code is required' }),
   city: z.string({ required_error: 'City is required' }),
   country: z.string({ required_error: 'Country is required' }),
+  userId: z.string().optional(),
+  guestId: z.string().optional(),
+  createdAt: z.date({ required_error: 'Created at is required' }),
+  isPaid: z.boolean(),
+  isDelivered: z.boolean(),
+  isSent: z.boolean(),
 });
 
 export const OrderShippingSchema = OrderSchema.pick({
@@ -18,6 +24,10 @@ export const OrderShippingSchema = OrderSchema.pick({
   postCode: true,
   city: true,
   country: true,
+  guestId: true,
+  userId: true,
+  createdAt: true,
+  isPaid: true,
 });
 
 export type Order = z.infer<typeof OrderSchema>;
