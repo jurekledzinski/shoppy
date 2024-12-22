71b67d4b5babe180e25dd9a206a57ecf19447952
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-22 17:41:14 +0100

    Update in place order section component

--- components/pages/place-order/PlaceOrderSection.tsx
+++ components/pages/place-order/PlaceOrderSection.tsx
@@ -1,6 +1,10 @@
 import { PlaceOrderSectionProps } from './types';
 import styles from './PlaceOrderSection.module.css';
 
-export const PlaceOrderSection = ({}: PlaceOrderSectionProps) => {
-  return <section className={styles.section}></section>;
+export const PlaceOrderSection = ({
+  children,
+  orderData,
+}: PlaceOrderSectionProps) => {
+  console.log('orderData place order section', orderData);
+  return <section className={styles.section}>{children}</section>;
 };
