a0eb758ec98d50dd246d19a4378a7b166c903604
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-22 17:40:53 +0100

    Update in place order page

--- app/shipping/place-order/page.tsx
+++ app/shipping/place-order/page.tsx
@@ -1,19 +1,80 @@
 import { auth } from '@/auth';
+import { cookies, headers } from 'next/headers';
+import { getDomain, tokenVerify } from '@/app/_helpers';
+import { Order } from '@/models';
 import { PlaceOrderSection } from '@/components/pages';
+import { ReadonlyHeaders } from 'next/dist/server/web/spec-extension/adapters/headers';
+import { tryCatch } from '@/helpers';
+import { steps } from '@/data';
+import { Step, Stepper } from '@/components/shared';
 
-type SearchParams = Promise<{ orderId: string }>;
+const fetchOrder = tryCatch<Order>(
+  async (url: string, headers?: ReadonlyHeaders) => {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers,
+      next: { revalidate: 3600, tags: ['get_order'] },
+    });
 
-const PlaceOrder = async (props: { searchParams: SearchParams }) => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return await response.json();
+  }
+);
+
+const secretGuest = process.env.GUEST_SECRET!;
+const secretStepper = process.env.STEPPER_SECRET!;
+
+const PlaceOrder = async () => {
   const session = await auth();
-  const searchParams = await props.searchParams;
-  //   const orderId = searchParams.orderId;
-  console.log('searchParams PlaceOrder page', searchParams);
+  const domain = await getDomain();
+  const headersFetch = await headers();
+  const cookieStore = await cookies();
+  const guestCookie = cookieStore.get('guestId') ?? null;
+  const stepperCookie = cookieStore.get('stepper') ?? null;
 
-  const orderId = searchParams.orderId;
-  console.log('orderId', orderId);
-  console.log('session PlaceOrder page', session);
+  const guestCookieDecoded = guestCookie
+    ? await tokenVerify<{ value: string }>(guestCookie.value, secretGuest)
+    : null;
 
-  return <PlaceOrderSection userData={[]}>PlaceOrder page</PlaceOrderSection>;
+  const stepperCookieDecoded = stepperCookie
+    ? await tokenVerify<{ value: { allowed: string; completed: string[] } }>(
+        stepperCookie.value,
+        secretStepper
+      )
+    : null;
+
+  const urlGetOrder = `${domain}/api/v1/order`;
+  const resOrder =
+    guestCookieDecoded || session
+      ? await fetchOrder(urlGetOrder, headersFetch)
+      : null;
+
+  const completedSteps = stepperCookieDecoded
+    ? stepperCookieDecoded.payload.value.completed
+    : [];
+
+  return (
+    <PlaceOrderSection
+      orderData={resOrder && resOrder.success ? resOrder.data : null}
+    >
+      <Stepper>
+        {steps.map((step) => {
+          return (
+            <Step
+              key={step.label}
+              icon={step.icon}
+              label={step.label}
+              path={step.path}
+              completed={completedSteps.includes(step.path)}
+            />
+          );
+        })}
+      </Stepper>
+    </PlaceOrderSection>
+  );
 };
 
 export default PlaceOrder;
