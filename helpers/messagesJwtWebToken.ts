e636b11ae211d198945302913129d9f4eabdd4ce
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-22 17:44:07 +0100

    Remove json web token error

--- helpers/messagesJwtWebToken.ts
+++ helpers/messagesJwtWebToken.ts
@@ -2,8 +2,6 @@ export const transformMessage = (name: string) => {
   switch (name) {
     case 'TokenExpiredError':
       return 'Invalid or expired link, please try one more time';
-    case 'JsonWebTokenError':
-      return 'Invalid or expired link, please try one more time';
     case 'CredentialsSignin':
       return 'Incorrect credentials';
     case 'AccessDenied':
