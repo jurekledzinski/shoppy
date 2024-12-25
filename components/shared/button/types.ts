e78324caad08e3bbfcc49c7fbf76b81ba88722f7
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-25 20:34:32 +0100

    Extend button compoent to extra property form

--- components/shared/button/types.ts
+++ components/shared/button/types.ts
@@ -4,6 +4,7 @@ export type ButtonProps = {
   className?: string;
   children?: React.ReactNode;
   disabled?: boolean;
+  form?: string;
   text?: React.ReactNode;
   type?: 'button' | 'submit' | 'reset' | undefined;
   onClick?: MouseEventHandler<HTMLButtonElement>;
