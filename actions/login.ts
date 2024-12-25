68bc08a0f565afee39a97479d1f61266971dcf54
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2024-12-25 21:55:52 +0100

    Add stepper cookie when login from option checkout

--- actions/login.ts
+++ actions/login.ts
@@ -1,12 +1,23 @@
 'use server';
-
-import { LoginUserSchema } from '@/models';
-import { signIn } from '@/auth';
 import { actionTryCatch } from '@/helpers';
+import { cookies } from 'next/headers';
+import { createToken } from '@/lib';
+import { LoginUserSchema } from '@/models';
+import { setCookieStepper } from '@/app/_helpers';
+import { signIn } from '@/auth';
+
+const secretStepper = process.env.STEPPER_SECRET!;
+const expireStepperToken = process.env.EXPIRE_STEPPER_TOKEN!;
+
+const payloadStepper = { allowed: '/shipping', completed: ['/'] };
 
 export const login = actionTryCatch(
   async (prevState: unknown, formData: FormData) => {
+    const cookieStore = await cookies();
     const body = Object.fromEntries(formData);
+
+    const paramsCheckout = body.params;
+    delete body.params;
 
     const parsedData = LoginUserSchema.parse(body);
 
@@ -15,6 +26,17 @@ export const login = actionTryCatch(
       password: parsedData.password,
       redirect: false,
     });
+
+    if (paramsCheckout === 'login' || paramsCheckout === 'register') {
+      const tokenStepper = await createToken(
+        payloadStepper,
+        secretStepper,
+        expireStepperToken
+      );
+
+      const expiresIn = new Date(Date.now() + 30 * 60 * 1000);
+      setCookieStepper(cookieStore, tokenStepper, expiresIn);
+    }
 
     return { message: 'Login success', success: true };
   }
