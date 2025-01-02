e87f2daa5da5768518c33111c76a4edefbb367ff
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-03 00:15:16 +0100

    Add message type jwt expired

--- helpers/messagesJwtWebToken.ts
+++ helpers/messagesJwtWebToken.ts
@@ -1,5 +1,7 @@
 export const transformMessage = (name: string) => {
   switch (name) {
+    case 'JWTExpired':
+      return "Your url link isn't valid anymore, request new one";
     case 'TokenExpiredError':
       return 'Invalid or expired link, please try one more time';
     case 'CredentialsSignin':
