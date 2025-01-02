7b48281fa3c6f42bac1a30f4728356a77023bf87
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-03 00:20:15 +0100

    Cart provider update

--- store/cart/CartProvider.tsx
+++ store/cart/CartProvider.tsx
@@ -1,15 +1,9 @@
 'use client';
-import { CartProviderProps, CartState, CartStoreContext } from './types';
+import { CartProviderProps, CartStoreContext } from './types';
 import { cartReducer } from './CartReducer';
-import { getItemFromLocalStorage, setItemToLocalStorage } from '@/helpers';
+// import { getItemFromLocalStorage, setItemToLocalStorage } from '@/helpers';
 
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useMemo,
-  useReducer,
-} from 'react';
+import { createContext, useContext, useMemo, useReducer } from 'react';
 
 export const initialState = {
   cart: {
@@ -39,23 +33,39 @@ const CartProvider = ({ children }: CartProviderProps) => {
     [state, dispatch]
   );
 
-  useEffect(() => {
-    if (state.cart.totalAmountCart) {
-      setItemToLocalStorage<CartState['cart']>('cart', state.cart);
-    }
-  }, [state]);
+  //   useEffect(() => {
+  //     if (state.cart.totalAmountCart) {
+  //       setItemToLocalStorage<CartState['cart']>('cart', state.cart);
+  //     }
+  //   }, [state]);
 
-  useEffect(() => {
-    const navigationEntries = window.performance.getEntriesByType('navigation');
-    const amountNavEntries = navigationEntries.length > 0;
-    const isReloadPage =
-      (navigationEntries[0] as PerformanceNavigationTiming).type === 'reload';
-    const localData = getItemFromLocalStorage('cart', 'null');
+  //   useEffect(() => {
+  //     const navigationEntries = window.performance.getEntriesByType('navigation');
+  //     const amountNavEntries = navigationEntries.length > 0;
+  //     const isReloadPage =
+  //       (navigationEntries[0] as PerformanceNavigationTiming).type === 'reload';
+  //     const localData = getItemFromLocalStorage('cart', 'null');
 
-    if (amountNavEntries && isReloadPage && localData) {
-      dispatch({ type: 'SET_CART', payload: localData });
-    }
-  }, []);
+  //     if (amountNavEntries && isReloadPage && localData) {
+  //       dispatch({ type: 'SET_CART', payload: localData });
+  //     }
+  //   }, []);
+
+  //   useSyncCart({
+  //     guestUser: value.guestUser,
+  //     sessionUser: value.userSession,
+  //     state: cartData.state.cart,
+  //     onGuest: useCallback(() => {
+  //       // call action to create and update cart for guest
+  //     }, []),
+  //     onNoUser: useCallback(() => {
+  //       //   const localData = JSON.stringify(localStorage.getItem('cart') || 'null');
+  //       // call to save state cart in localstorage also on refresh page
+  //     }, []),
+  //     onUser: useCallback(() => {
+  //       // call action to create and update cart for user logged in
+  //     }, []),
+  //   });
 
   return <CartContext.Provider value={values}>{children}</CartContext.Provider>;
 };
