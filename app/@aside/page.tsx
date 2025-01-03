0275d2a0aa8422f09b9f1441c9ec186590885eb8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-03 13:47:26 +0100

    Add function fetch to get cart if user logged in or guest user

--- app/@aside/page.tsx
+++ app/@aside/page.tsx
@@ -4,7 +4,7 @@ import { cookies, headers } from 'next/headers';
 import { getDomain } from '../_helpers';
 import { ReadonlyHeaders } from 'next/dist/server/web/spec-extension/adapters/headers';
 import { tryCatch } from '@/helpers';
-import { UserRegister } from '@/models';
+import { Cart, UserRegister } from '@/models';
 import { verifyToken } from '@/lib';
 
 const fetchUser = tryCatch<Omit<UserRegister, 'password' | 'email'>>(
@@ -23,25 +23,49 @@ const fetchUser = tryCatch<Omit<UserRegister, 'password' | 'email'>>(
   }
 );
 
+const fetchCart = tryCatch<Omit<Cart, 'cartId'>>(
+  async (url: string, headers?: ReadonlyHeaders) => {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers,
+      next: { revalidate: 3600, tags: ['get_cart'] },
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return await response.json();
+  }
+);
+
 const secretGuest = process.env.GUEST_SECRET!;
 
 const Aside = async () => {
   const session = await auth();
   const domain = await getDomain();
-  const userHeaders = await headers();
+  const allHeaders = await headers();
   const cookieStore = await cookies();
 
   const guestCookie = cookieStore.get('guestId') ?? null;
 
   const urlGetUser = `${domain}/api/v1/user?id=${session?.user.id}`;
-  const resUser = session ? await fetchUser(urlGetUser, userHeaders) : null;
+  const resUser = session ? await fetchUser(urlGetUser, allHeaders) : null;
 
   const guestCookieDecoded = guestCookie
     ? await verifyToken<{ value: string }>(guestCookie.value, secretGuest)
     : null;
 
+  const urlGetCart = `${domain}/api/v1/cart`;
+
+  const resCart =
+    session || guestCookieDecoded
+      ? await fetchCart(urlGetCart, allHeaders)
+      : null;
+
   return (
     <MainAside
+      cartData={resCart && resCart.success ? resCart.data : null}
       guestId={guestCookieDecoded ? guestCookieDecoded.payload.value : null}
       userData={resUser && resUser.success ? resUser.data : null}
     />
