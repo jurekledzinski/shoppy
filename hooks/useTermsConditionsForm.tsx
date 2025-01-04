81c49d54df2ff9334530013454feea82f657ce15
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-04 21:14:01 +0100

    Pass cart data to checkout action

--- hooks/useTermsConditionsForm.tsx
+++ hooks/useTermsConditionsForm.tsx
@@ -1,10 +1,11 @@
-import { Order } from '@/models';
+import { Cart, Order } from '@/models';
 import { startTransition } from 'react';
 import { TermsConditionsFormInputs } from '@/components/pages';
 import { useForm } from 'react-hook-form';
 import { useResetForm } from './useResetForm';
 
 type useTermsConditionsFormProps = {
+  cartData: Cart | null;
   defaultData: Order | null;
   formAction: (payload: FormData) => void;
   isPending: boolean;
@@ -13,6 +14,7 @@ type useTermsConditionsFormProps = {
 };
 
 export const useTermsConditionsForm = ({
+  cartData,
   defaultData,
   formAction,
   isPending,
@@ -29,6 +31,12 @@ export const useTermsConditionsForm = ({
     const formData = new FormData();
     formData.set('_id', defaultData?._id ?? '');
     formData.set('termsConditions', data.termsConditions.toString());
+    if (cartData) {
+      formData.set('cartId', cartData._id ?? '');
+      formData.set('products', JSON.stringify(cartData.products));
+      formData.set('totalAmountCart', cartData.totalAmountCart.toString());
+      formData.set('totalPriceCart', cartData.totalPriceCart.toString());
+    }
 
     startTransition(() => {
       formAction(formData);
