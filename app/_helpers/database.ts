a41e9086b3753e57b8f9a1845cad786b1c29a6ec
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-07 00:36:24 +0100

    Add more helper functions for database

--- app/_helpers/database.ts
+++ app/_helpers/database.ts
@@ -6,6 +6,8 @@ import {
   OrderCheckout,
   OrderPlaceOrder,
   OrderShipping,
+  OrderSuccess,
+  ProductCard,
 } from '@/models';
 
 export const updateShipping = async (
@@ -47,7 +49,7 @@ export const updateCheckoutOrder = async (
   const { _id, ...rest } = parsedData;
   const id = new ObjectId(_id);
 
-  await collection.updateOne({ _id: id }, { $set: { ...rest } });
+  return await collection.updateOne({ _id: id }, { $set: { ...rest } });
 };
 
 export const updateCart = async (
@@ -74,8 +76,59 @@ export const updateCart = async (
 
 export const updateCartExpiryAt = async (
   collection: Collection<Omit<Cart, '_id'>>,
-  guestId: string
+  guestId: string,
+  expiryAt: Date
 ) => {
-  const expiryAt = new Date(Date.now() + 30 * 60 * 1000);
   await collection.updateOne({ guestId }, { $set: { expiryAt } });
+};
+
+export const deleteCart = async (
+  collection: Collection<Cart>,
+  userIdKey: 'userId' | 'guestId',
+  id: string
+) => {
+  await collection.deleteOne({ [userIdKey]: id });
+};
+
+export const updateSuccessOrder = async (
+  collection: Collection<Omit<Order, '_id'>>,
+  parsedData: OrderSuccess,
+  orderId: string
+) => {
+  await collection.updateOne(
+    { _id: new ObjectId(orderId) },
+    { $set: { ...parsedData }, $unset: { expiryAt: '' } }
+  );
+};
+
+export const updateProductsQuantity = async (
+  collection: Collection<Omit<ProductCard, '_id'>>,
+  products: Cart['products']
+) => {
+  const bulkOperations = products.map(({ _id, quantity }) => ({
+    updateOne: {
+      filter: { _id: new ObjectId(_id) },
+      update: { $inc: { onStock: -quantity } },
+    },
+  }));
+
+  await collection.bulkWrite(bulkOperations);
+};
+
+export const deleteOrder = async (
+  collection: Collection<Omit<Order, '_id'>>,
+  orderId: string
+) => {
+  await collection.deleteOne({ _id: new ObjectId(orderId) });
+};
+
+export const updateExpiryAtOrder = async (
+  collection: Collection<Omit<Order, '_id'>>,
+  orderId: string,
+  expiryAt: Date
+) => {
+  await collection.updateOne(
+    { _id: new ObjectId(orderId) },
+    { $set: { expiryAt } }
+  );
 };
