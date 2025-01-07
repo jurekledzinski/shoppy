5b5d8c1466646635ed9f1d0c75d0601cdb9edec1
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-07 14:43:58 +0100

    Remove comments in cancel page

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -1,7 +1,3 @@
-// remove guestId i stepper jeśli zalogowany jako guest
-// remove stepper jeśli zalogowany jako user
-// show modal ask to clear cart or not if not extend stepper and guestId for 1hour
-
 import { SectionCancel } from '@/components/pages';
 
 type SearchParams = Promise<{ orderId: string }>;
