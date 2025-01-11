19d54bd82353ebb4bc80c89c2b342545c697ed24
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-11 01:18:49 +0100

    Update success page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,42 +1,36 @@
-// fetch cart ok
-// dodaj cart do order ok
-// zmien isPaid na true w order także ok
-// zmien ilość produktów kupionych w bazie danych i  revalidate tag by pobrało świeże dane ok
-// usun cart z kolekcji carts dla danego userId albo guestId ok
-// clear koszyk ok
-
 import { auth } from '@/auth';
-import { fetchCart, getDomain } from '../_helpers';
-import { cookies, headers } from 'next/headers';
+import { cookies } from 'next/headers';
 import { verifyToken } from '@/lib';
 import { SectionSuccess } from '@/components/pages';
 
 type SearchParams = Promise<{ orderId: string }>;
 
 const secretGuest = process.env.GUEST_SECRET!;
+const secretStepper = process.env.STEPPER_SECRET!;
 
 const Success = async ({ searchParams }: { searchParams: SearchParams }) => {
   const orderId = (await searchParams).orderId;
   const session = await auth();
-  const domain = await getDomain();
-  const allHeaders = await headers();
   const cookieStore = await cookies();
   const guestCookie = cookieStore.get('guestId') ?? null;
+  const stepperCookie = cookieStore.get('stepper') ?? null;
 
   const guestCookieDecoded = guestCookie?.value
     ? await verifyToken<{ value: string }>(guestCookie.value, secretGuest)
     : null;
 
-  const urlGetCart = `${domain}/api/v1/cart`;
-
-  const resCart =
-    session || guestCookieDecoded
-      ? await fetchCart(urlGetCart, allHeaders)
-      : null;
+  const stepperCookieDecoded = stepperCookie?.value
+    ? await verifyToken<{ value: { allowed: string; completed: string[] } }>(
+        stepperCookie.value,
+        secretStepper
+      )
+    : null;
 
   return (
     <SectionSuccess
-      cartData={resCart && resCart.success ? resCart.data : null}
+      isGuestLogin={Boolean(guestCookieDecoded)}
+      isStepperLogin={Boolean(stepperCookieDecoded)}
+      isUserLogIn={Boolean(session)}
       orderId={orderId}
     />
   );
