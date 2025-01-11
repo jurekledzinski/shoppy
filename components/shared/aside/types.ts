e6bf64c4c14e5c6a0f813f73ef86b020a3c45a9e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-11 01:25:45 +0100

    Remove cart data fetch from server component

--- components/shared/aside/types.ts
+++ components/shared/aside/types.ts
@@ -1,7 +1,6 @@
-import { Cart, UserRegister } from '@/models';
+import { UserRegister } from '@/models';
 
 export type AsideProps = {
-  cartData: Cart | null;
   userData: Omit<UserRegister, 'password' | 'email'> | null;
   guestId: string | null;
 };
