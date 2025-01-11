0d3875ee324243dc393571008a294bbb438c9f97
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-11 01:10:50 +0100

    Add more things to logout action

--- actions/logout.ts
+++ actions/logout.ts
@@ -1,8 +1,15 @@
 'use server';
-import { logOut } from '@/auth';
 import { actionTryCatch } from '@/helpers';
+import { cookies } from 'next/headers';
+import { deleteCookie } from '@/app/_helpers';
+import { logOut } from '@/auth';
 
 export const logout = actionTryCatch(async () => {
+  const cookieStore = await cookies();
+  const cookieStepper = cookieStore.get('stepper');
+
+  if (cookieStepper) deleteCookie(cookieStore, 'stepper');
+
   await logOut({ redirect: false });
 
   return { message: 'Logout success', success: true };
