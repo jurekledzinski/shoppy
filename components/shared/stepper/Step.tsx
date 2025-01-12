64462a11cf086ff45b74a69c1814583ae68ea961
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-12 15:45:29 +0100

    Add active state step

--- components/shared/stepper/Step.tsx
+++ components/shared/stepper/Step.tsx
@@ -1,14 +1,19 @@
+'use client';
 import Link from 'next/link';
+import styles from './Stepper.module.css';
 import { StepperIcon } from './StepperIcon';
 import { StepperLabel } from './StepperLabel';
 import { StepProps } from './types';
-import styles from './Stepper.module.css';
+import { usePathname } from 'next/navigation';
 
 export const Step = ({ completed, icon, label, path }: StepProps) => {
+  const currentPathname = usePathname();
   return (
     <Link
       href={`${path}`}
-      className={`${styles.step} ${completed ? styles.completed : styles.step}`}
+      className={`${styles.step} ${
+        completed ? styles.completed : styles.step
+      } ${path === currentPathname ? styles.active : styles.step}`}
     >
       <StepperIcon icon={icon} />
       <StepperLabel label={label} />
