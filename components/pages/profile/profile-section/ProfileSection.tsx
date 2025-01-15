aa02de8643d35604c95bd5bd173eab30713988cb
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-15 16:26:59 +0100

    Refactor delete accoun user

--- components/pages/profile/profile-section/ProfileSection.tsx
+++ components/pages/profile/profile-section/ProfileSection.tsx
@@ -6,58 +6,61 @@ import { ChangePasswordForm, UpdateProfileForm } from '../forms';
 import { logOut } from '@/auth';
 import { ProfileSectionProps } from './types';
 import { showToast } from '@/helpers';
-import { startTransition, useActionState } from 'react';
-import { useChangePasswordForm, useUpdateProfileForm } from '@/hooks';
+import { startTransition, useCallback } from 'react';
+import { useCart } from '@/store/cart';
 import { useRouter } from 'next/navigation';
+
+import {
+  useActionStateAndReset,
+  useChangePasswordForm,
+  useUpdateProfileForm,
+} from '@/hooks';
 
 import {
   changeUserPassword,
   deleteUserAccount,
   updateUserProfile,
+  clearDataDeleteAccount,
 } from '@/actions';
 
 export const ProfileSection = ({ children, userData }: ProfileSectionProps) => {
   const router = useRouter();
+  const { dispatch } = useCart();
 
-  const [stateDeleteAccount, formActionDeleteAccount, isPendingDeleteAccount] =
-    useActionState(deleteUserAccount, {
-      message: '',
-      success: false,
+  const { action: actionDelete, resetStateAction: resetStateActionDelete } =
+    useActionStateAndReset({
+      fnAction: deleteUserAccount,
     });
 
-  const [stateProfile, formActionProfile, isPendingProfile] = useActionState(
-    updateUserProfile,
-    {
-      message: '',
-      success: false,
-    }
-  );
+  const { action: actionProfile, resetStateAction: resetStateActionProfile } =
+    useActionStateAndReset({
+      fnAction: updateUserProfile,
+    });
 
-  const [statePassword, formActionPassword, isPendingPassword] = useActionState(
-    changeUserPassword,
-    {
-      message: '',
-      success: false,
-    }
-  );
+  const { action: actionPassword, resetStateAction: resetStateActionPassword } =
+    useActionStateAndReset({
+      fnAction: changeUserPassword,
+    });
 
   const { methodsUpdateProfile, onSubmitUpdateProfile } = useUpdateProfileForm({
     defaults: userData,
-    formAction: formActionProfile,
-    isPending: isPendingProfile,
-    isSuccess: stateProfile.success,
+    formAction: actionProfile.formAction,
+    isPending: actionProfile.isPending,
+    isSuccess: actionProfile.state.success,
     onSuccess: () => {
-      showToast(stateProfile.message);
+      resetStateActionProfile();
+      showToast(actionProfile.state.message);
     },
   });
 
   const { methodsChangePassword, onSubmitChangePassword } =
     useChangePasswordForm({
-      formAction: formActionPassword,
-      isPending: isPendingPassword,
-      isSuccess: statePassword.success,
+      formAction: actionPassword.formAction,
+      isPending: actionPassword.isPending,
+      isSuccess: actionPassword.state.success,
       onSuccess: () => {
-        showToast(statePassword.message);
+        resetStateActionPassword();
+        showToast(actionPassword.state.message);
       },
     });
 
@@ -68,20 +71,20 @@ export const ProfileSection = ({ children, userData }: ProfileSectionProps) => {
         Update profile user
       </header>
       <UpdateProfileForm
-        isPending={isPendingProfile}
+        isPending={actionProfile.isPending}
         methods={methodsUpdateProfile}
         onSubmit={onSubmitUpdateProfile}
-        state={stateProfile}
+        state={actionProfile.state}
       />
 
       <header className={styles.headerChangePassword}>
         Change password user
       </header>
       <ChangePasswordForm
-        isPending={isPendingPassword}
+        isPending={actionPassword.isPending}
         methods={methodsChangePassword}
         onSubmit={onSubmitChangePassword}
-        state={statePassword}
+        state={actionPassword.state}
       />
 
       <header className={styles.headerDeleteAccount}>
@@ -89,22 +92,34 @@ export const ProfileSection = ({ children, userData }: ProfileSectionProps) => {
       </header>
 
       <ModalDelete
-        isPending={isPendingDeleteAccount}
-        isSuccess={stateDeleteAccount.success}
+        isPending={actionDelete.isPending}
+        isSuccess={actionDelete.state.success}
         classButton={stylesButton.buttonDelete}
         onConfirm={(e) => {
           e.preventDefault();
-          const formData = new FormData();
           startTransition(() => {
-            formActionDeleteAccount(formData);
+            actionDelete.formAction(new FormData());
           });
         }}
         title="Delete user account"
-        onSuccess={async () => {
+        onSuccess={useCallback(async () => {
+          dispatch({ type: 'CLEAR_CART' });
+
+          await clearDataDeleteAccount('', new FormData());
+
+          resetStateActionDelete();
+
           await logOut();
-          showToast(stateDeleteAccount.message);
+
+          showToast(actionDelete.state.message);
+
           router.replace('/');
-        }}
+        }, [
+          actionDelete.state.message,
+          dispatch,
+          router,
+          resetStateActionDelete,
+        ])}
         textButton="Delete account"
       >
         <p className={styles.modalDeleteText}>
@@ -112,8 +127,8 @@ export const ProfileSection = ({ children, userData }: ProfileSectionProps) => {
         </p>
       </ModalDelete>
 
-      {!stateDeleteAccount.success && stateDeleteAccount.message && (
-        <Alert>{stateDeleteAccount.message}</Alert>
+      {!actionDelete.state.success && actionDelete.state.message && (
+        <Alert>{actionDelete.state.message}</Alert>
       )}
     </Section>
   );
