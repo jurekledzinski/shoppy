6d87482017e57d9a6469bbf22dda8efda8072ebb
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-04 15:50:30 +0100

    Refactor contact  server action

--- actions/contact.ts
+++ actions/contact.ts
@@ -1,16 +1,14 @@
 'use server';
+
 import contactEmailTemplate from '../templates/contactEmail.hbs';
-import fs from 'fs';
-import juice from 'juice';
-import nodemailer from 'nodemailer';
-import path from 'path';
 import { actionTryCatch } from '@/helpers';
 import { ContactEmailSchema } from '@/models';
+
+import { mergeTemplate, nodemailerTransporter, setMailOptions } from '@/lib';
 
 export const contact = actionTryCatch(
   async (prevState: unknown, formData: FormData) => {
     const body = Object.fromEntries(formData);
-
     const parsedData = ContactEmailSchema.parse(body);
 
     const currentYear = new Date().getFullYear();
@@ -23,31 +21,18 @@ export const contact = actionTryCatch(
       date,
     });
 
-    const cssPath = path.join(process.cwd(), 'templates/contactEmail.css');
+    const htmlWithCss = await mergeTemplate(
+      'templates/contactEmail.css',
+      htmlContent
+    );
 
-    const cssContent = fs.readFileSync(cssPath, 'utf-8');
+    const transporter = await nodemailerTransporter();
 
-    const htmlWithCss = juice.inlineContent(htmlContent, cssContent);
-
-    const transporter = nodemailer.createTransport({
-      host: process.env.HOST_EMAIL,
-      port: process.env.PORT_PROVIDER_EMAIL,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.PASSWORD_USER,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    } as nodemailer.TransportOptions);
-
-    const mailOptions = {
-      from: `Shoopy shop <${process.env.EMAIL_USER}>`,
-      to: process.env.EMAIL_SENDTO,
-      subject: 'Contact email',
-      html: htmlWithCss,
-    };
+    const mailOptions = await setMailOptions(
+      'Shoopy shop',
+      'Contact email',
+      htmlWithCss
+    );
 
     await transporter.sendMail(mailOptions);
 
