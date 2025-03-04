77d4288b5ccdc3ec7a3a78bc6dc4e546a398e2bf
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-04 15:53:57 +0100

    Refactor success order server action

--- actions/successOrder.ts
+++ actions/successOrder.ts
@@ -1,105 +1,55 @@
 'use server';
-import { Cart, Order, OrderSuccessSchema, ProductCard } from '@/models';
+import { Cart, OrderSuccessSchema } from '@/models';
 import {
   connectDBAction,
-  getAuthToken,
-  getCollectionDb,
-  verifyToken,
   deleteCart,
   deleteCookie,
   updateProductsQuantity,
   updateSuccessOrder,
+  getSessionData,
+  validationData,
 } from '@/lib';
-import { cookies, headers } from 'next/headers';
-import { errorMessageAction } from '@/helpers';
 import { revalidateTag } from 'next/cache';
-
-const secretGuest = process.env.GUEST_SECRET!;
-const secretStepper = process.env.STEPPER_SECRET!;
 
 export const successOrder = connectDBAction(
   async (prevState: unknown, formData: FormData) => {
-    const cookieStore = await cookies();
-    const headersData = await headers();
-    const cookieGuest = cookieStore.get('guestId');
-    const cookieStepper = cookieStore.get('stepper');
+    const { cookieGuest, cookieStepper, cookieStore, token } =
+      await getSessionData();
 
     const body = Object.fromEntries(formData);
     const cart = JSON.parse(body.cart as string) as Cart;
     const orderId = body.orderId as string;
-
-    const token = await getAuthToken({ headers: headersData });
 
     const parsedData = OrderSuccessSchema.parse({
       cart,
       isPaid: true,
     });
 
-    if (!token && !cookieGuest && !cookieStepper) {
-      return errorMessageAction('Unauthorized');
-    }
-
-    const collectionOrders = getCollectionDb<Omit<Order, '_id'>>('orders');
-    if (!collectionOrders) return errorMessageAction('Internal server error');
-
-    const collectionCarts = getCollectionDb<Omit<Cart, '_id'>>('carts');
-    if (!collectionCarts) return errorMessageAction('Internal server error');
-
-    const collectionProducts = getCollectionDb<ProductCard>('products');
-    if (!collectionProducts) {
-      return errorMessageAction('Internal server error');
-    }
+    const result = await validationData(cookieGuest, token, cookieStepper);
+    if ('message' in result) return result;
+    const { collectionCarts, collectionOrders, collectionProducts, guest } =
+      result;
 
     if (!token && cookieGuest && cookieStepper) {
-      const dataGuest = await verifyToken<{ value: string }>(
-        cookieGuest.value,
-        secretGuest
-      );
-
-      await verifyToken<{
-        value: { allowed: string; completed: string[] };
-      }>(cookieStepper.value, secretStepper);
-
-      await updateSuccessOrder(collectionOrders, parsedData, orderId);
-
-      await updateProductsQuantity(collectionProducts, cart.products);
-
-      await deleteCart(collectionCarts, 'guestId', dataGuest.payload.value);
-
+      await deleteCart(collectionCarts, 'guestId', guest);
       deleteCookie(cookieStore, 'guestId');
-      deleteCookie(cookieStore, 'stepper');
-
-      revalidateTag('get_order');
-      revalidateTag('get_products');
-
-      return {
-        message: 'Order updated successful',
-        success: true,
-      };
     }
 
     if (token && !cookieGuest && cookieStepper) {
-      await verifyToken<{
-        value: { allowed: string; completed: string[] };
-      }>(cookieStepper.value, secretStepper);
-
-      await updateSuccessOrder(collectionOrders, parsedData, orderId);
-
-      await updateProductsQuantity(collectionProducts, cart.products);
-
       await deleteCart(collectionCarts, 'userId', token.id as string);
-
-      revalidateTag('get_order');
-      revalidateTag('get_products');
-
-      deleteCookie(cookieStore, 'stepper');
-
-      return {
-        message: 'Order updated successful',
-        success: true,
-      };
     }
 
-    return errorMessageAction('Unauthorized');
+    await updateProductsQuantity(collectionProducts, cart.products);
+    await updateSuccessOrder(collectionOrders, parsedData, orderId);
+
+    deleteCookie(cookieStore, 'stepper');
+
+    revalidateTag('get_order');
+    revalidateTag('get_products');
+
+    return {
+      message: 'Order updated successful',
+      success: true,
+    };
   }
 );
