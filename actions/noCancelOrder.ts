ba6ef6715f9555f37022aec84b26877a30387c6a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-04 15:52:36 +0100

    Refactor no cancel order server action

--- actions/noCancelOrder.ts
+++ actions/noCancelOrder.ts
@@ -1,114 +1,63 @@
 'use server';
-import { Cart, Order } from '@/models';
-import { cookies, headers } from 'next/headers';
-import { errorMessageAction } from '@/helpers';
 import { revalidateTag } from 'next/cache';
 
 import {
   connectDBAction,
   createToken,
-  getAuthToken,
-  getCollectionDb,
-  verifyToken,
   getExpireInCookie,
   setCookieGuestId,
   setCookieStepper,
   updateCartExpiryAt,
   updateExpiryAtOrder,
+  getAuthSecrets,
+  stepperStepsFinish,
+  getSessionData,
+  validationData,
 } from '@/lib';
-
-const payloadStepper = {
-  allowed: '/shipping/place-order/details-order',
-  completed: [
-    '/',
-    '/shipping',
-    '/shipping/place-order',
-    '/shipping/place-order/details-order',
-  ],
-};
-
-const secretGuest = process.env.GUEST_SECRET!;
-const secretStepper = process.env.STEPPER_SECRET!;
-const expireGuestToken = process.env.EXPIRE_GUEST_TOKEN!;
-const expireStepperToken = process.env.EXPIRE_STEPPER_TOKEN!;
 
 export const noCancelOrder = connectDBAction(
   async (prevState: unknown, formData: FormData) => {
-    const cookieStore = await cookies();
-    const headersData = await headers();
-    const cookieGuest = cookieStore.get('guestId');
-    const cookieStepper = cookieStore.get('stepper');
+    const AUTH = await getAuthSecrets();
+    const STEPPER_PAYLOAD = await stepperStepsFinish();
+
+    const { cookieGuest, cookieStepper, cookieStore, token } =
+      await getSessionData();
 
     const body = Object.fromEntries(formData);
     const orderId = body.orderId as string;
 
-    const token = await getAuthToken({ headers: headersData });
-
-    if (!token && !cookieGuest && !cookieStepper) {
-      return errorMessageAction('Unauthorized');
-    }
-
-    const collectionOrders = getCollectionDb<Omit<Order, '_id'>>('orders');
-    if (!collectionOrders) return errorMessageAction('Internal server error');
-
-    const collectionCarts = getCollectionDb<Omit<Cart, '_id'>>('carts');
-    if (!collectionCarts) return errorMessageAction('Internal server error');
+    const result = await validationData(cookieGuest, token, cookieStepper);
+    if ('message' in result) return result;
+    const { collectionCarts, collectionOrders, guest } = result;
 
     const expiresIn = getExpireInCookie(45 * 60 * 1000);
+
     const tokenStepper = await createToken(
-      payloadStepper,
-      secretStepper,
-      expireStepperToken
+      STEPPER_PAYLOAD,
+      AUTH.SECRET_STEPPER,
+      AUTH.EXPIRE_STEPPER_TOKEN
     );
 
     if (!token && cookieGuest && cookieStepper) {
-      const dataGuest = await verifyToken<{ value: string }>(
-        cookieGuest.value,
-        secretGuest
-      );
-
-      await verifyToken<{
-        value: { allowed: string; completed: string[] };
-      }>(cookieStepper.value, secretStepper);
-
       const tokenGuest = await createToken(
-        dataGuest.payload.value,
-        secretGuest,
-        expireGuestToken
+        guest,
+        AUTH.SECRET_GUEST,
+        AUTH.EXPIRE_GUEST_TOKEN
       );
 
-      await updateCartExpiryAt(
-        collectionCarts,
-        dataGuest.payload.value,
-        expiresIn
-      );
-
+      await updateCartExpiryAt(collectionCarts, guest, expiresIn);
       await updateExpiryAtOrder(collectionOrders, orderId, expiresIn);
 
       setCookieGuestId(cookieStore, tokenGuest, expiresIn);
-      setCookieStepper(cookieStore, tokenStepper, expiresIn);
-
-      revalidateTag('get_order');
-
-      return {
-        message: 'Order updated successful',
-        success: true,
-      };
     }
 
-    if (token && !cookieGuest && cookieStepper) {
-      await verifyToken<{
-        value: { allowed: string; completed: string[] };
-      }>(cookieStepper.value, secretStepper);
+    setCookieStepper(cookieStore, tokenStepper, expiresIn);
 
-      setCookieStepper(cookieStore, tokenStepper, expiresIn);
+    revalidateTag('get_order');
 
-      return {
-        message: 'Order updated successful',
-        success: true,
-      };
-    }
-
-    return errorMessageAction('Unauthorized');
+    return {
+      message: 'Order updated successful',
+      success: true,
+    };
   }
 );
