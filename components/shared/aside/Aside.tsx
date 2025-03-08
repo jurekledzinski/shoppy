9af6c4abea563307eac987d84463b3e9e094d72f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-08 20:51:26 +0100

    Refactor Aside

--- components/shared/aside/Aside.tsx
+++ components/shared/aside/Aside.tsx
@@ -1,93 +1,57 @@
 'use client';
 import styles from './Aside.module.css';
 import { AsideProps } from './types';
-import { extendGuestSession, guestCheckout, userCheckout } from '@/actions';
-import { ModalWarning } from '../modal-warning';
-import { startTransition, useActionState, useCallback, useEffect } from 'react';
-import { initialState, updateSyncCart, useCart } from '@/store/cart';
+import { extendGuestSession } from '@/actions';
+import { removeQueryUrl, showToast } from '@/helpers';
+import { startTransition, useActionState, useCallback } from 'react';
 import { useAside } from '@/store/aside';
+import { useCart } from '@/store/cart';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useSessionUser } from '@/store/session';
-import { getSession } from 'next-auth/react';
 
 import {
-  controlAside,
-  redirectWithQueries,
-  removeQueryUrl,
-  showToast,
-} from '@/helpers';
+  AsidePanels,
+  useAsideHandlers,
+  ModalGuestSession,
+} from '@/components/pages';
+
 import {
-  useActionStateAndReset,
   useLoadResetPasswordForm,
   useSetCartOnRefresh,
   useSetUserSession,
-  useSetTimeoutController,
+  initialState,
 } from '@/hooks';
 
-import {
-  CartPanel,
-  ContactPanel,
-  ForgetPasswordPanel,
-  MenuPanel,
-  LoginPanel,
-  RegisterPanel,
-  ResetPasswordPanel,
-  ProccedCheckoutPanel,
-} from '@/components/pages';
-
 export const Aside = ({ guestId, userData }: AsideProps) => {
-  const context = useAside();
-  const sessionUser = useSessionUser();
-  const actionElement = context.type;
-  const stateOpen = context.value;
-  const optionCheckout = context.checkout;
-  const userId = userData?._id ?? '';
-  const userName = userData?.name ?? '';
   const searchParams = useSearchParams();
   const paramActionType = searchParams.get('action_type');
   const guestUserExpire = searchParams.get('guest-user-expired');
+  const context = useAside();
+  const sessionUser = useSessionUser();
+  const userId = userData?._id ?? '';
+  const userName = userData?.name ?? '';
   const router = useRouter();
-  const { dispatch, state } = useCart();
+  const { dispatch, state: cartState } = useCart();
 
   const [stateExtendGuestSession, formAction, isPending] = useActionState(
     extendGuestSession,
-    {
-      message: '',
-      success: false,
-    }
+    initialState
   );
 
-  const {
-    action: actionUserCheckout,
-    resetStateAction: resetStateActionUserCheckout,
-  } = useActionStateAndReset({
-    fnAction: userCheckout,
-    onResetAction: () => {
-      context.onChange(actionElement, false);
-      setControlTimeout(() => router.replace('/shipping'), 200);
+  const asideHandlers = useAsideHandlers({
+    cartState,
+    context,
+    onDispatch: (type, payload) => {
+      if (type === 'CLEAR_CART') dispatch({ type });
+      if (type === 'SET_CART' && payload) dispatch({ type, payload });
     },
-  });
-
-  useEffect(() => {
-    if (actionUserCheckout.state.success && !actionUserCheckout.isPending) {
-      resetStateActionUserCheckout();
-    }
-  }, [actionUserCheckout, resetStateActionUserCheckout]);
-
-  const { action, resetStateAction } = useActionStateAndReset({
-    fnAction: guestCheckout,
-    onResetAction: () => {
-      context.onChange(actionElement, false);
-      updateSyncCart(state, userData?._id, guestId);
-      setControlTimeout(() => router.replace('/shipping'), 200);
+    onRedirect: (url) => {
+      router.replace(url);
+      router.refresh();
     },
+    onShowToast: (message) => showToast(message),
+    user: { guestId: guestId ?? '', userName, userId },
   });
-
-  useEffect(() => {
-    if (action.state.success && !action.isPending) {
-      resetStateAction();
-    }
-  }, [action, resetStateAction]);
 
   useLoadResetPasswordForm({ context, paramActionType });
 
@@ -96,23 +60,21 @@ export const Aside = ({ guestId, userData }: AsideProps) => {
     sessionUser,
     userId,
     onLoggedGuest: useCallback(
-      (guestId) => {
-        sessionUser.onSetValue('guestUser', guestId);
-      },
+      (guestId) => sessionUser.onSetValue('guestUser', guestId),
       [sessionUser]
     ),
-    onNotLoggedGuest: useCallback(() => {
-      sessionUser.onSetValue('guestUser', null);
-    }, [sessionUser]),
+    onNotLoggedGuest: useCallback(
+      () => sessionUser.onSetValue('guestUser', null),
+      [sessionUser]
+    ),
     onLoggedUser: useCallback(
-      (userId) => {
-        sessionUser.onSetValue('userSession', userId);
-      },
+      (userId) => sessionUser.onSetValue('userSession', userId),
       [sessionUser]
     ),
-    onNotLoggedUser: useCallback(() => {
-      sessionUser.onSetValue('userSession', null);
-    }, [sessionUser]),
+    onNotLoggedUser: useCallback(
+      () => sessionUser.onSetValue('userSession', null),
+      [sessionUser]
+    ),
   });
 
   useSetCartOnRefresh({
@@ -127,207 +89,38 @@ export const Aside = ({ guestId, userData }: AsideProps) => {
     guestSession: guestId,
   });
 
-  const setControlTimeout = useSetTimeoutController();
-
   return (
     <>
-      <ModalWarning
+      <ModalGuestSession
         isPending={isPending}
         isOpen={guestUserExpire === 'true'}
         isSuccess={stateExtendGuestSession.success && !isPending}
-        title="Session will expire in 10 minutes"
-        confirm="Extend session"
         onConfirm={() => {
           startTransition(() => formAction(new FormData()));
           const newPath = removeQueryUrl(searchParams, 'guest-user-expired');
-          setControlTimeout(() => router.replace(newPath), 1000);
+          setTimeout(() => router.replace(newPath), 1000);
         }}
-      >
-        <p>
-          Your guest session will expire in 10 minutes. Please click extend
-          session button to stay logged in as guest user.
-        </p>
-      </ModalWarning>
+      />
+
       <aside
         className={`${styles.aside} ${
           context.value ? styles.show : styles.aside
         }`}
       >
-        {context.type === 'menu' ? (
-          <MenuPanel
-            actionElement={actionElement}
-            context={context}
-            stateOpen={stateOpen}
-            user={{ id: userId, name: userName }}
-            onSuccessAction={() => {
-              dispatch({ type: 'CLEAR_CART' });
-              const newUrl = redirectWithQueries();
-
-              router.replace(newUrl);
-              router.refresh();
-            }}
-          />
-        ) : context.type === 'cart' ? (
-          <CartPanel
-            actionElement={actionElement}
-            context={context}
-            data={state}
-            dispatch={dispatch}
-            guestId={guestId}
-            onSuccess={() => {
-              if (userName && userId) {
-                return startTransition(() =>
-                  actionUserCheckout.formAction(new FormData())
-                );
-              }
-
-              context.onChange(actionElement, false);
-              router.replace('/shipping');
-            }}
-            stateOpen={stateOpen}
-            userId={userId}
-            userName={userName}
-            state={state}
-            isPending={actionUserCheckout.isPending}
-          />
-        ) : context.type === 'contact' ? (
-          <ContactPanel
-            onSuccessAction={(message) => {
-              showToast(message);
-              context.onChange(actionElement, false);
-            }}
-          />
-        ) : context.type === 'login' ? (
-          <LoginPanel
-            onRedirectForgetPassword={(e) => {
-              e.preventDefault();
-              controlAside(
-                context,
-                'forget-password',
-                actionElement,
-                stateOpen
-              );
-            }}
-            onRedirectRegister={(e) => {
-              e.preventDefault();
-              controlAside(context, 'register', actionElement, stateOpen);
-            }}
-            onSuccessAction={async () => {
-              showToast('Login successful');
-              context.onChange(actionElement, false);
-              const updatedSession = await getSession();
-              const idUser = updatedSession?.user.id;
-
-              if (state.cart.totalAmountCart) {
-                const resultCart = await updateSyncCart(state, idUser, guestId);
-                dispatch({
-                  type: 'SET_CART',
-                  payload: resultCart?.payload ?? initialState.cart,
-                });
-              }
-
-              if (optionCheckout === 'login') {
-                setControlTimeout(() => router.replace('/shipping'), 200);
-                return;
-              }
-
-              const newUrl = redirectWithQueries();
-              router.push(newUrl);
-              router.refresh();
-            }}
-            optionCheckout={optionCheckout}
-          />
-        ) : context.type === 'register' ? (
-          <RegisterPanel
-            onRedirectLogin={(e) => {
-              e.preventDefault();
-              controlAside(context, 'login', actionElement, stateOpen);
-            }}
-            onSuccessAction={() => {
-              showToast('Register successful');
-              context.onChange(actionElement, false);
-              if (optionCheckout === 'register') {
-                controlAside(
-                  context,
-                  'login',
-                  actionElement,
-                  stateOpen,
-                  'login'
-                );
-              }
-            }}
-          />
-        ) : context.type === 'forget-password' ? (
-          <ForgetPasswordPanel
-            onSuccessAction={(message) => {
-              showToast(message, 10000);
-              context.onChange(actionElement, false);
-            }}
-          />
-        ) : context.type === 'reset_password' ? (
-          <>
-            <ResetPasswordPanel
-              onCancel={(e) => {
-                e.preventDefault();
-                const url = new URL(window.location.href);
-                url.search = '';
-                router.refresh();
-                router.replace(url.pathname);
-
-                setControlTimeout(
-                  () => context.onChange(actionElement, false),
-                  1000
-                );
-              }}
-              onSuccessAction={(message) => {
-                showToast(message);
-                const url = new URL(window.location.href);
-                url.search = '';
-                router.refresh();
-                router.replace(url.pathname);
-
-                setControlTimeout(
-                  () =>
-                    controlAside(context, 'login', actionElement, stateOpen),
-                  1000
-                );
-              }}
-            />
-          </>
-        ) : context.type === 'procced-checkout-options' ? (
-          <ProccedCheckoutPanel
-            isPending={action.isPending}
-            onCancelAction={() => {
-              context.onChange(actionElement, false);
-            }}
-            onContinueAction={(name) => {
-              const options = {
-                guest: () =>
-                  startTransition(() => action.formAction(new FormData())),
-                register: () => {
-                  controlAside(
-                    context,
-                    'register',
-                    actionElement,
-                    stateOpen,
-                    'register'
-                  );
-                },
-                login: () => {
-                  controlAside(
-                    context,
-                    'login',
-                    actionElement,
-                    stateOpen,
-                    'login'
-                  );
-                },
-              };
-
-              options[name as keyof typeof options]();
-            }}
-          />
-        ) : null}
+        <AsidePanels
+          cartState={cartState}
+          context={context}
+          isPending={
+            asideHandlers.isPendingUserCheckout ||
+            asideHandlers.isPendingGuestCheckout
+          }
+          onCancel={asideHandlers.onCancelHandler}
+          onContinue={asideHandlers.onContinueHandler}
+          onDispatch={(type, payload) => dispatch({ type, payload })}
+          onRedirect={asideHandlers.onRedirectHandler}
+          onSuccess={asideHandlers.onSuccessHandler}
+          user={{ guestId: guestId ?? '', userName, userId }}
+        />
       </aside>
     </>
   );
