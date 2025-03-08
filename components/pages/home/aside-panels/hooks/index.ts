6986665c2fdcf557594c1a864ff188a4a4c602d1
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-08 21:02:36 +0100

    Changes in reexport custom hooks

---
+++ components/pages/home/aside-panels/hooks/index.ts
@@ -0,0 +1,6 @@
+export * from './useAsideHandlers';
+export * from './useCancelPanel';
+export * from './useContinuePanel';
+export * from './useRedirect';
+export * from './useSuccessAction';
+export * from './types';
