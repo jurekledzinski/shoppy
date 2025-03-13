7a0540b2bff96fbdcdf97441a261cf8a048f1d89
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-13 22:43:44 +0100

    Remove query key from fetch brands in api route

--- app/api/v1/brands/route.ts
+++ app/api/v1/brands/route.ts
@@ -1,20 +1,14 @@
 import 'server-only';
 import { connectDB, getCollectionDb } from '@/lib';
 import { errorMessage } from '@/helpers';
-import { NextRequest } from 'next/server';
 import { Brand } from '@/models';
 
-export const GET = connectDB(async (request: NextRequest) => {
-  const { searchParams } = new URL(request.url);
-  const category = searchParams.get('category');
-
-  if (!category || category === 'undefined') return errorMessage(404);
-
+export const GET = connectDB(async () => {
   const collection = getCollectionDb<Brand>('brands');
 
   if (!collection) return errorMessage(500);
 
-  const brands = await collection.find<Brand>({ category }).toArray();
+  const brands = await collection.find<Brand>({}).toArray();
 
   if (!brands) return errorMessage(404);
 
