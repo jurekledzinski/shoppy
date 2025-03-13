53a60e263f24686644767793b8aa049fb40eb177
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-13 10:32:52 +0100

    Change loaders in loading pages

--- app/success/loading.tsx
+++ app/success/loading.tsx
@@ -1,11 +1,10 @@
 import styles from '@/components/shared/section/Section.module.css';
-import stylesLoader from '@/components/shared/loader/Loader.module.css';
 import { Loader, Section } from '@/components/shared';
 
 const Loading = () => {
   return (
     <Section className={styles.sectionCentered}>
-      <Loader className={stylesLoader.loaderBigger} />
+      <Loader position="center" size={30} />
     </Section>
   );
 };
