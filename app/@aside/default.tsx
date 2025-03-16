d98f6ac2cb8fe9ef433690c3b5e46b6457960e2d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-16 23:12:39 +0100

    Refactor fetch data on server for aside

--- app/@aside/default.tsx
+++ app/@aside/default.tsx
@@ -1,29 +1,13 @@
 import { Aside as MainAside } from '@/components/shared';
-import { auth } from '@/auth';
-import { cookies, headers } from 'next/headers';
-import { fetchUser, tokenVerify } from '@/lib';
-import { getDomain } from '@/helpers';
-
-const secretGuest = process.env.GUEST_SECRET!;
+import { getAsidetData } from '@/lib';
 
 const Aside = async () => {
-  const session = await auth();
-  const domain = await getDomain();
-  const allHeaders = await headers();
-  const cookieStore = await cookies();
-  const guestCookie = cookieStore.get('guestId') ?? null;
-
-  const urlGetUser = `${domain}/api/v1/user?id=${session?.user.id}`;
-  const resUser = session ? await fetchUser(urlGetUser, allHeaders) : null;
-
-  const guestCookieDecoded = guestCookie?.value
-    ? await tokenVerify<{ value: string }>(guestCookie.value, secretGuest)
-    : null;
+  const { guestId, userData } = await getAsidetData();
 
   return (
     <MainAside
-      guestId={guestCookieDecoded ? guestCookieDecoded.payload.value : null}
-      userData={resUser && resUser.success ? resUser.data : null}
+      guestId={guestId ? guestId : null}
+      userData={userData ? userData : null}
     />
   );
 };
