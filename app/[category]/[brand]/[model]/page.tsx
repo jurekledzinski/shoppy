635e8ec96c982867bc75b903d67e424c8b847593
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-16 23:13:12 +0100

    Refactor fetch data on server for detaisl product page

--- app/[category]/[brand]/[model]/page.tsx
+++ app/[category]/[brand]/[model]/page.tsx
@@ -1,10 +1,8 @@
-import { auth } from '@/auth';
 import { Breadcrumb, Breadcrumbs } from '@/components/shared';
 import { DetailsProductSection } from '@/components/pages';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { fetchDetailsProduct, fetchProductReviews, fetchUser } from '@/lib';
-import { getBreadcrumbsDetails, getDomain } from '@/helpers';
-import { headers } from 'next/headers';
+import { getBreadcrumbsDetails } from '@/helpers';
+import { getDetailsProductData } from '@/lib';
 
 type Params = Promise<{ category: string; brand: string; model: string }>;
 type SearchParams = Promise<{ id: string }>;
@@ -13,32 +11,27 @@ const DetailsProduct = async (props: {
   params: Params;
   searchParams: SearchParams;
 }) => {
-  const session = await auth();
-  const domain = await getDomain();
-
   const { brand, category, model } = await props.params;
-  const queryId = (await props.searchParams).id;
+  const productId = (await props.searchParams).id;
 
-  const breadcrumbs = getBreadcrumbsDetails([category, brand, model, queryId]);
+  const { productData, reviewData, userData } = await getDetailsProductData(
+    productId
+  );
 
-  const urlGetProduct = `${domain}/api/v1/product?id=${queryId}`;
-
-  const resDetailsProduct = await fetchDetailsProduct(urlGetProduct);
-
-  const urlGetProductReviews = `${domain}/api/v1/review?product_id=${queryId}`;
-  const resReviews = await fetchProductReviews(urlGetProductReviews);
-
-  const userHeaders = await headers();
-  const urlGetUser = `${domain}/api/v1/user?id=${session?.user.id}`;
-  const resUser = session ? await fetchUser(urlGetUser, userHeaders) : null;
+  const breadcrumbs = getBreadcrumbsDetails([
+    category,
+    brand,
+    model,
+    productId,
+  ]);
 
   return (
     <DetailsProductSection
-      dataProduct={resDetailsProduct.success ? resDetailsProduct.data : null}
-      dataReviews={resReviews.success ? resReviews.data : []}
-      dataUser={resUser && resUser.success ? resUser.data : null}
-      {...(!resReviews.success && {
-        errorReviews: resReviews,
+      dataProduct={productData ? productData : null}
+      dataReviews={reviewData ? reviewData : []}
+      dataUser={userData ? userData : null}
+      {...(!reviewData && {
+        errorReviews: "Couldn't fetch reviews, please try later.",
       })}
     >
       <Breadcrumbs>
