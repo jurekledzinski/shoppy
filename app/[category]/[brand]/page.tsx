73285069833ae1ecb561fe889316eab9e56a57c8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-16 23:13:01 +0100

    Refactor fetch data on server for products

--- app/[category]/[brand]/page.tsx
+++ app/[category]/[brand]/page.tsx
@@ -1,20 +1,18 @@
 import { Breadcrumb, Breadcrumbs } from '@/components/shared';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { fetchProducts } from '@/lib';
-import { getBreadcrumbsProductsPage, getDomain } from '@/helpers';
+import { getBreadcrumbsProductsPage } from '@/helpers';
+import { getProductsData } from '@/lib';
 import { ProductsSection } from '@/components/pages';
 
 type Params = Promise<{ category: string; brand: string }>;
 
 const Products = async (props: { params: Params }) => {
-  const domain = await getDomain();
   const params = await props.params;
+  const productsData = await getProductsData(params.category, params.brand);
   const breadcrumbs = getBreadcrumbsProductsPage(params.category, params.brand);
-  const urlGetProducts = `${domain}/api/v1/products?category=${params.category}&brand=${params.brand}`;
-  const resProducts = await fetchProducts(urlGetProducts);
 
   return (
-    <ProductsSection data={resProducts.success ? resProducts.data ?? [] : []}>
+    <ProductsSection data={productsData ? productsData : []}>
       <Breadcrumbs>
         {breadcrumbs.map((segment, index) => {
           return (
