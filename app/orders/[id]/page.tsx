f159b6d3730db808feafc0e396a27e0fe1534e0b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-03-16 23:13:21 +0100

    Refactor fetch data on server for orders page

--- app/orders/[id]/page.tsx
+++ app/orders/[id]/page.tsx
@@ -1,25 +1,21 @@
-import { auth } from '@/auth';
 import { Breadcrumb, Breadcrumbs } from '@/components/shared';
+import { endpoints, fetchUserOrders, getSessionData } from '@/lib';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { fetchUserOrders } from '@/lib';
 import { getBreadcrumbsOrders, getDomain } from '@/helpers';
-import { headers } from 'next/headers';
 import { OrdersSection } from '@/components/pages';
 
 type Params = Promise<{ id: string }>;
 
 const Orders = async (props: { params: Params }) => {
-  const session = await auth();
+  const { session, headersData } = await getSessionData();
   const domain = await getDomain();
-  const allHeaders = await headers();
   const params = await props.params;
-  const breadcrumbs = getBreadcrumbsOrders(params.id);
-
-  const urlGetUserOrders = `${domain}/api/v1/orders`;
 
   const resOrders = session
-    ? await fetchUserOrders(urlGetUserOrders, allHeaders)
+    ? await fetchUserOrders(endpoints.orders(domain), headersData)
     : null;
+
+  const breadcrumbs = getBreadcrumbsOrders(params.id);
 
   return (
     <OrdersSection
